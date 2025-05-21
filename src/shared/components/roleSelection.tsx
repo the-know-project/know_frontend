e2a2324a0bd44721af6bae5f0a4226060ea39f2a
3illBaby <thrillanon@outlook.com>
2025-05-21 23:19:45 +0100

    chore(roleselection.tsx): removed wrongly configured state management

--- src/shared/components/roleSelection.tsx
+++ src/shared/components/roleSelection.tsx
@@ -1,75 +1,77 @@
+"use client";
+
 import Image from "next/image";
 import { LuUserRound } from "react-icons/lu";
 import Link from "next/link";
-import { useDispatch } from "react-redux";
-import { setRole } from "../../redux/slice/authSlice";
-import Router from "next/router";
+import { useRouter } from "next/navigation";
 
 export default function SelectRole() {
-  const dispatch = useDispatch();
-  const router = Router
+  const router = useRouter();
 
   const handleSelectRole = (selectedRole: string) => {
-    dispatch(setRole(selectedRole));
+    console.log(selectedRole);
     router.push("/auth/signup");
   };
 
-
   return (
-    <div className="min-h-screen grid grid-cols-1 md:grid-cols-2">
-      
-      <div className="flex flex-col justify-center px-8 md:px-16 py-12 bg-white">
-        
-        <div className="flex justify-between items-center mb-10">
+    <div className="grid min-h-screen grid-cols-1 md:grid-cols-2">
+      <div className="flex flex-col justify-center bg-white px-8 py-12 md:px-16">
+        <div className="mb-10 flex items-center justify-between">
           <Image src="/Know-Logo.png" alt="Logo" width={60} height={60} />
           <p className="text-sm text-neutral-500">
             Already have an account?{" "}
-            <a href="/login" className="text-blue-600 hover:underline">Log In</a>
+            <a href="/login" className="text-blue-600 hover:underline">
+              Log In
+            </a>
           </p>
         </div>
 
-        <h1 className="text-3xl text-black font-semibold mb-2">Sign Up</h1>
-        <p className="text-sm text-neutral-500 mb-8">
-          To get started, please select your primary role. You can switch between roles at any time in your profile.
+        <h1 className="mb-2 text-3xl font-semibold text-black">Sign Up</h1>
+        <p className="mb-8 text-sm text-neutral-500">
+          To get started, please select your primary role. You can switch
+          between roles at any time in your profile.
         </p>
 
-        
         <div className="space-y-4">
           <Link href={"/auth/signup"}>
-          <button onClick={() => handleSelectRole("ARTIST")} className="w-full cursor-pointer border border-b-neutral-300 p-4 rounded-lg flex items-start gap-3">
-            <div className="h-[20px]  md:h-[52px] md:w-[52px] w-[20px] flex items-center justify-center  rounded-[24px] bg-gray-400/35">
-            <LuUserRound  className="mt-1 text-2xl  md:text-3xl text-neutral-700" />
-            </div>
-            
-            <div className="text-left">
-              <p className="font-medium text-black text-lg">Artist</p>
-              <p className="text-sm text-neutral-500">
-                Showcase your art to a global audience and expand your reach.
-              </p>
-            </div>
-          </button>
+            <button
+              onClick={() => handleSelectRole("ARTIST")}
+              className="flex w-full cursor-pointer items-start gap-3 rounded-lg border border-b-neutral-300 p-4"
+            >
+              <div className="flex h-[20px] w-[20px] items-center justify-center rounded-[24px] bg-gray-400/35 md:h-[52px] md:w-[52px]">
+                <LuUserRound className="mt-1 text-2xl text-neutral-700 md:text-3xl" />
+              </div>
+
+              <div className="text-left">
+                <p className="text-lg font-medium text-black">Artist</p>
+                <p className="text-sm text-neutral-500">
+                  Showcase your art to a global audience and expand your reach.
+                </p>
+              </div>
+            </button>
           </Link>
-          
 
-          <button onClick={() => handleSelectRole("BUYER")} className="w-full cursor-pointer p-4 rounded-lg flex items-start gap-3 hover:border-black transition">
-           <div className="h-[20px] md:h-[52px] w-[20px] md:w-[52px] flex items-center justify-center  rounded-[24px] bg-gray-400/35">
-            <LuUserRound  className="mt-1 text-2xl  md:text-3xl text-neutral-700" />
-
-           </div>
+          <button
+            onClick={() => handleSelectRole("BUYER")}
+            className="flex w-full cursor-pointer items-start gap-3 rounded-lg p-4 transition hover:border-black"
+          >
+            <div className="flex h-[20px] w-[20px] items-center justify-center rounded-[24px] bg-gray-400/35 md:h-[52px] md:w-[52px]">
+              <LuUserRound className="mt-1 text-2xl text-neutral-700 md:text-3xl" />
+            </div>
             <div className="text-left">
-              <p className="font-medium text-black text-lg">Buyer</p>
+              <p className="text-lg font-medium text-black">Buyer</p>
               <p className="text-sm text-neutral-500">
-                Discover unique artworks, connect with artists, and build your collection.
+                Discover unique artworks, connect with artists, and build your
+                collection.
               </p>
             </div>
           </button>
         </div>
       </div>
 
-    
-      <div className="hidden md:block relative">
+      <div className="relative hidden md:block">
         <Image
-          src="/roleselect.png" 
+          src="/roleselect.png"
           alt="Signup Visual"
           layout="fill"
           objectFit="cover"
