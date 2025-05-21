f8d2ce459724026fb8e5002f1b4c5c203cd231a9
3illBaby <thrillanon@outlook.com>
2025-05-21 18:56:21 +0100

    chore(eslint.config.njs): modified rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,7 @@ const eslintConfig = [
   ...compat.config({
     extends: [("next/core-web-vitals", "next/typescript")],
     rules: {
-      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "warn",
       "@typescript-eslint/no-require-imports": "off",
     },
