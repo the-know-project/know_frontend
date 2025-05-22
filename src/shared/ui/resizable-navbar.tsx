47a0c0904dbcbc3200b699dfdbab84131c3f9259
3illBaby <thrillanon@outlook.com>
2025-05-22 17:10:56 +0100

    style(resizeable-navbar.tsx): removed backdrop

--- src/shared/ui/resizable-navbar.tsx
+++ src/shared/ui/resizable-navbar.tsx
@@ -205,7 +205,7 @@ export const MobileNavMenu = ({
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
           className={cn(
-            "absolute inset-x-0 top-16 z-50 flex w-full flex-col items-start justify-start gap-4 rounded-lg bg-neutral-950 px-4 py-8 shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] dark:bg-neutral-950",
+            "absolute inset-x-0 top-18 z-50 flex w-full flex-col items-start justify-start gap-4 rounded-lg bg-neutral-950 px-4 py-8 shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] dark:bg-neutral-950",
             className,
           )}
         >
@@ -242,7 +242,7 @@ export const NavbarLogo = () => {
 };
 
 export const NavbarButton = ({
-  as: Tag = "a",
+  as: Tag = "div",
   children,
   colors,
   className,
@@ -253,7 +253,7 @@ export const NavbarButton = ({
   className?: string;
   colors: string[];
 } & (
-  | React.ComponentPropsWithoutRef<"a">
+  | React.ComponentPropsWithoutRef<"div">
   | React.ComponentPropsWithoutRef<"button">
 )) => {
   return (
