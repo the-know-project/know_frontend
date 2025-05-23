82e3bb3ca46d9907bcad1b71bfe27885e4590047
3illBaby <thrillanon@outlook.com>
2025-05-23 19:20:16 +0100

    chore(tiltsection.tsx): extended tilt section

--- src/shared/components/tiltsection.tsx
+++ src/shared/components/tiltsection.tsx
@@ -1,11 +1,14 @@
-import Image from "next/image";
+"use client";
+
+import Image, { StaticImageData } from "next/image";
 import { TitleText } from "../layout/header";
+import useIsVisible from "../hooks/use-is-visible";
 
 type TiltSectionProps = {
   role: string;
   title: string;
   subtitle: string;
-  imgSrc: string;
+  imgSrc: string | StaticImageData;
   reverse?: boolean;
 };
 
@@ -16,9 +19,10 @@ export default function TiltSection({
   imgSrc,
   reverse = false,
 }: TiltSectionProps) {
+  const { ref, isVisible } = useIsVisible();
   return (
     <section
-      className={`flex flex-col items-center justify-between gap-10 bg-white px-6 py-12 text-black md:flex-row ${
+      className={`flex flex-col items-center justify-between gap-10 bg-neutral-50 px-6 py-12 text-black md:flex-row ${
         reverse ? "md:flex-row-reverse" : ""
       }`}
     >
@@ -30,18 +34,21 @@ export default function TiltSection({
           <h2 className="font-helvetica mb-4 text-2xl font-medium capitalize md:text-3xl">
             {title}
           </h2>
+          <p className="font-grotesk text-md max-w-prose text-gray-950 md:text-lg">
+            {subtitle}
+          </p>
         </TitleText>
-        <p className="font-grotesk text-base text-gray-950">{subtitle}</p>
       </div>
 
-      <div className="flex flex-1 justify-center">
-        <div className="rotate-[-6deg] transform overflow-hidden rounded-xl">
+      <div className="flex w-full flex-1 justify-center">
+        <div className="relative rotate-[-6deg] transform overflow-hidden rounded-xl">
           <Image
+            ref={ref}
             src={imgSrc}
             alt="Gallery Image"
             width={400}
             height={300}
-            className="object-fit rotate-[-6deg] transform"
+            className={`object-fit relat rotate-[-6deg] transform ${isVisible ? "motion-preset-expand motion-duration-1000" : "opacity-0"}`}
           />
         </div>
       </div>
