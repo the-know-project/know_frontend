2eddd9cf650eedd71b5a8473eef0f00471542f4f
3illBaby <thrillanon@outlook.com>
2025-05-23 19:20:58 +0100

    feat(useisvisbile.tsx): impl visibility hook
    for animations

---
+++ src/shared/hooks/use-is-visible.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+
+const useIsVisible = () => {
+  const ref = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsVisible(entry.isIntersecting);
+      },
+      { threshold: 0.1 },
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, []);
+
+  return { ref, isVisible };
+};
+
+export default useIsVisible;
