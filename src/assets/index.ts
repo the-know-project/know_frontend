f96554b81dcbab017f08924c54cf182866568dbf
3illBaby <thrillanon@outlook.com>
2025-05-23 19:18:52 +0100

    chore(assets): added about assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,5 +2,8 @@ import hero1 from "./hero1.png";
 import hero2 from "./hero2.jpeg";
 import hero3 from "./hero3.png";
 import hero4 from "./hero4.png";
+import for_artist from "./forArtist.png";
+import for_buyers from "./for-buyers.png";
+import secure_tx from "./secure-transaction.png";
 
-export { hero1, hero2, hero3, hero4 };
+export { hero1, hero2, hero3, hero4, for_artist, for_buyers, secure_tx };
