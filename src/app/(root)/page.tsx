24ae699d881876c2625d184d5ffc20d466f4ef83
3illBaby <thrillanon@outlook.com>
2025-05-23 18:06:39 +0100

    refactor(page.tsx): moved tilt to standalone about component

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import About from "@/src/shared/components/about";
 import Hero from "@/src/shared/components/hero";
 import JoinCTA from "@/src/shared/components/joincta";
 import TiltSection from "@/src/shared/components/tiltsection";
@@ -6,31 +7,16 @@ import Nav from "@/src/shared/layout/navbar";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <div className="relative">
         <div className="absolute w-full">
           <Nav />
         </div>
         <Hero />
       </div>
-      <TiltSection
-        title="Showcase your work, manage your portfolio, and reach global buyers."
-        subtitle="Showcase your unique creations, manage a professional portfolio with ease, and connect with a global audience."
-        imgSrc="/forArtist.png"
-      />
-      <TiltSection
-        title="Showcase your work, manage your portfolio, and reach global buyers."
-        subtitle="Showcase your unique creations, manage a professional portfolio with ease, and connect with a global audience."
-        imgSrc="/forArtist.png"
-        reverse
-      />
-      <TiltSection
-        title="Showcase your work, manage your portfolio, and reach global buyers."
-        subtitle="Showcase your unique creations, manage a professional portfolio with ease, and connect with a global audience."
-        imgSrc="/forArtist.png"
-      />
+      <About />
       <JoinCTA />
       <Footer />
-    </div>
+    </>
   );
 }
