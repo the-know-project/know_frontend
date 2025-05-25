2925ca0053a236acbab0033672fad5b9360d8de1
3illBaby <thrillanon@outlook.com>
2025-05-25 16:15:02 +0100

    refactor(about.tsx): refactored about page

--- src/shared/components/about.tsx
+++ src/shared/components/about.tsx
@@ -9,19 +9,22 @@ const About = () => {
         title="Showcase your work, manage your portfolio, and reach global buyers."
         subtitle="Showcase your unique creations, manage a professional portfolio with ease, and connect with a global audience."
         imgSrc={for_artist}
+        glow_color="purple-glow"
       />
       <TiltSection
         role="For Buyers"
-        title="Showcase your work, manage your portfolio, and reach global buyers."
-        subtitle="Showcase your unique creations, manage a professional portfolio with ease, and connect with a global audience."
+        title="Discover unique art, express interest, and support artists worldwide."
+        subtitle="From emerging talents to established artists, find unique art and foster a global creative community."
         imgSrc={for_buyers}
         reverse
+        glow_color="orange-glow"
       />
       <TiltSection
-        role="Secure Payments"
-        title="Showcase your work, manage your portfolio, and reach global buyers."
-        subtitle="Showcase your unique creations, manage a professional portfolio with ease, and connect with a global audience."
+        role="Secure Transactions"
+        title="Safe and seamless payments with every transaction."
+        subtitle="Enjoy the flexibility of paying in your preferred currency while benefiting from robust security measures that safeguard your financial data."
         imgSrc={secure_tx}
+        glow_color="green-glow"
       />
     </section>
   );
