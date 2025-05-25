acfd577afb6e8d09e8cd4310ab6146a80ce06749
3illBaby <thrillanon@outlook.com>
2025-05-25 16:36:27 +0100

    chore(joincta.tsx): added id

--- src/shared/components/joincta.tsx
+++ src/shared/components/joincta.tsx
@@ -2,14 +2,15 @@ import Image from "next/image";
 
 export default function JoinCTA() {
   return (
-    <section className="relative bg-gradient-to-br from-[#0a0f3c] to-[#08104d] text-white px-6 py-20 overflow-hidden">
-   
-      <div className="absolute right-0 top-0 bottom-0 w-full max-w-[500px]  pointer-events-none flex justify-end pr-10">
-       
+    <section
+      id="community"
+      className="relative overflow-hidden bg-gradient-to-br from-[#0a0f3c] to-[#08104d] px-6 py-20 text-white"
+    >
+      <div className="pointer-events-none absolute top-0 right-0 bottom-0 flex w-full max-w-[500px] justify-end pr-10">
         {Array.from({ length: 5 }).map((_, i) => (
           <div key={i} className="ml-3">
             <Image
-              src="/Vector 1.png" 
+              src="/Vector 1.png"
               alt="Vector"
               width={100}
               height={400}
@@ -19,18 +20,20 @@ export default function JoinCTA() {
         ))}
       </div>
 
-     
-      <div className="relative z-10  md:ml-11 max-w-3xl mx-auto ">
-        <p className="text-sm uppercase text-neutral-300 mb-2">Ready to join?</p>
-        <h2 className="text-3xl md:text-4xl font-bold mb-6">
-          Showcase your talent or discover <br /> incredible art — start your journey today.
+      <div className="relative z-10 mx-auto max-w-3xl md:ml-11">
+        <p className="font-bebas mb-2 text-lg text-neutral-300 uppercase">
+          Ready to join?
+        </p>
+        <h2 className="font-helvetica mb-6 max-w-prose text-3xl font-bold capitalize md:text-4xl">
+          Showcase your talent or discover incredible art — start your journey
+          today.
         </h2>
 
-        <form className="flex items-center  justify-center gap-2 max-w-lg mx-auto">
+        <form className="mx-auto flex max-w-lg items-center justify-center gap-2">
           <input
             type="email"
             placeholder="Email Address"
-            className="input input-bordered w-full max-w-sm text-black bg-white"
+            className="input input-bordered w-full max-w-sm bg-white text-black"
           />
           <button className="btn btn-primary">Get Started</button>
         </form>
