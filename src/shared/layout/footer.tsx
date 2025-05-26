230b6feb87ac8995d2664d8333ce845300e57b84
3illBaby <thrillanon@outlook.com>
2025-05-26 10:46:47 +0100

    revert(footer.tsx): removed background

--- src/shared/layout/footer.tsx
+++ src/shared/layout/footer.tsx
@@ -9,59 +9,61 @@ import {
 
 export default function Footer() {
   return (
-    <footer className="flex w-full flex-col bg-black px-6 py-10 text-neutral-50">
-      <div className="h-[50px]">
-        <TextEffectWithExit text="Powered by stellar" />
-      </div>
-      <div className="flex flex-col items-start gap-5">
-        <div className="flex items-center gap-2">
-          <Image
-            src="/Know-Logo.png"
-            alt="Logo"
-            width={50}
-            height={50}
-            quality={100}
-          />
+    <footer className="flex w-full flex-col bg-black text-neutral-50">
+      <div className="flex w-full flex-col px-6 py-10">
+        <div className="h-[50px]">
+          <TextEffectWithExit text="Powered by stellar" />
         </div>
+        <div className="flex flex-col items-start gap-5">
+          <div className="flex items-center gap-2">
+            <Image
+              src="/Know-Logo.png"
+              alt="Logo"
+              width={50}
+              height={50}
+              quality={100}
+            />
+          </div>
 
-        <div className="font-grotesk flex flex-col gap-2 text-sm capitalize">
-          {INavItems.map((item, index) => (
-            <a key={index} href={item.link}>
-              {item.name}
+          <div className="font-grotesk lg:text-md flex flex-col gap-2 text-sm capitalize">
+            {INavItems.map((item, index) => (
+              <a key={index} href={item.link}>
+                {item.name}
+              </a>
+            ))}
+          </div>
+
+          <div className="mt-5 flex gap-4 text-lg">
+            <a href="#">
+              <IconBrandInstagram
+                width={30}
+                height={30}
+                color="white"
+                className="transition-all duration-200 hover:scale-105 active:scale-95"
+              />
             </a>
-          ))}
+            <a href="#">
+              <IconBrandX
+                width={30}
+                height={30}
+                color="white"
+                className="transition-all duration-200 hover:scale-105 active:scale-95"
+              />
+            </a>
+            <a href="#">
+              <IconBrandLinkedin
+                width={30}
+                height={30}
+                color="white"
+                className="transition-all duration-200 hover:scale-105 active:scale-95"
+              />
+            </a>
+          </div>
         </div>
 
-        <div className="mt-5 flex gap-4 text-lg">
-          <a href="#">
-            <IconBrandInstagram
-              width={30}
-              height={30}
-              color="white"
-              className="transition-all duration-200 hover:scale-105 active:scale-95"
-            />
-          </a>
-          <a href="#">
-            <IconBrandX
-              width={30}
-              height={30}
-              color="white"
-              className="transition-all duration-200 hover:scale-105 active:scale-95"
-            />
-          </a>
-          <a href="#">
-            <IconBrandLinkedin
-              width={30}
-              height={30}
-              color="white"
-              className="transition-all duration-200 hover:scale-105 active:scale-95"
-            />
-          </a>
+        <div className="font-grotesk mt-6 text-center text-sm text-neutral-200 capitalize">
+          &copy; {new Date().getFullYear()} All rights reserved.
         </div>
-      </div>
-
-      <div className="font-grotesk mt-6 text-center text-sm text-neutral-200 capitalize">
-        &copy; {new Date().getFullYear()} All rights reserved.
       </div>
     </footer>
   );
