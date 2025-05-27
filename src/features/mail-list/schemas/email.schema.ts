4120252ac92288f2d9616e7112341d996ba6fe3c
3illBaby <thrillanon@outlook.com>
2025-05-27 22:53:25 +0100

    chore(email.schema.ts): added response schema

--- src/features/mail-list/schemas/email.schema.ts
+++ src/features/mail-list/schemas/email.schema.ts
@@ -3,4 +3,9 @@ export const EmailSchema = z.object({
   email: z.string().email({ message: "Not an email" }),
 });
 
+export const AddToMailListResponseDto = z.object({
+  status: z.number(),
+  message: z.string(),
+});
+
 export type IAddToMailList = z.infer<typeof EmailSchema>;
