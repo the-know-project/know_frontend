32f62e4f24c6364032b4e940a0b037522ff56f28
3illBaby <thrillanon@outlook.com>
2025-05-27 22:18:25 +0100

    chore(environment.ts): added environment config

---
+++ src/config/environment.ts
@@ -0,0 +1,15 @@
+export const isProduction = () => {
+  return process.env.NODE_ENV === "production";
+};
+
+export const isDevelopment = () => {
+  return process.env.NODE_ENV === "development";
+};
+
+export const isTest = () => {
+  return process.env.NODE_ENV === "test";
+};
+
+export const getEnvironment = () => {
+  return process.env.NODE_ENV || "development";
+};
