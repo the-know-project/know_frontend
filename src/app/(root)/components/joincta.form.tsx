920b77dcd15e31a0aa29dab1b0aa2e5b114d5d6e
3illBaby <thrillanon@outlook.com>
2025-05-27 23:34:15 +0100

    feat(joincta.form.tsx): consumed hook

--- src/app/(root)/components/joincta.form.tsx
+++ src/app/(root)/components/joincta.form.tsx
@@ -11,26 +11,33 @@ import {
   FormMessage,
 } from "@/src/shared/ui/form";
 import { Input } from "@/src/shared/ui/input";
-import { IJoinCta, JoinCtaSchema } from "@/src/shared/schema/joincta.schema";
 import { NavbarButton } from "@/src/shared/ui/resizable-navbar";
+import {
+  EmailSchema,
+  IAddToMailList,
+} from "@/src/features/mail-list/schemas/email.schema";
+import { useAddToMailList } from "@/src/features/mail-list/hooks/use-add-to-mail-list";
+import Spinner from "@/src/shared/components/spinner";
 
 const JoinCtaForm = () => {
-  const form = useForm<IJoinCta>({
-    resolver: zodResolver(JoinCtaSchema),
+  const { mutateAsync: addToMailList, isPending } = useAddToMailList();
+  const form = useForm<IAddToMailList>({
+    resolver: zodResolver(EmailSchema),
     defaultValues: {
       email: "",
     },
   });
 
-  const handleCtaAction = (ctx: IJoinCta) => {
-    console.log(ctx);
+  const handleCtaAction = async (ctx: IAddToMailList) => {
+    const data = await addToMailList(ctx);
+    console.log(data);
   };
   return (
     <section className="flex w-full flex-row items-center justify-start sm:justify-center">
       <Form {...form}>
         <form
           onSubmit={form.handleSubmit(handleCtaAction)}
-          className="flex flex-row items-center gap-2"
+          className="relative flex flex-row items-center gap-2"
         >
           <FormField
             control={form.control}
@@ -39,12 +46,15 @@ const JoinCtaForm = () => {
               <FormItem>
                 <FormControl>
                   <Input
-                    className="font-grotesk placeholder:font-bebas !important placeholder:text-md flex w-full rounded-lg bg-neutral-100 text-neutral-700 shadow-sm placeholder:text-neutral-700 focus-visible:shadow-none focus-visible:ring-0 sm:placeholder:text-sm md:min-w-[400px]"
+                    className="font-bricolage placeholder:font-bebas !important placeholder:text-md flex w-full rounded-lg bg-neutral-100 text-neutral-700 shadow-sm placeholder:text-neutral-700 focus-visible:shadow-none focus-visible:ring-0 sm:placeholder:text-sm md:min-w-[400px]"
                     placeholder="Enter your email"
                     {...field}
                   />
                 </FormControl>
-                <FormMessage />
+
+                <div className="absolute left-3">
+                  <FormMessage className="font-bebas tracking-widest" />
+                </div>
               </FormItem>
             )}
           />
@@ -56,13 +66,19 @@ const JoinCtaForm = () => {
               className="font-bebas text-md group relative inline-flex h-9 w-fit items-center gap-1 rounded-lg bg-blue-500 px-2.5 py-1.5 font-medium text-nowrap text-white capitalize outline outline-[#fff2f21f] transition-all duration-200"
               type="submit"
             >
-              Get Started
-              <IconSend
-                width={20}
-                height={20}
-                color="white"
-                className="transiton-all duration-200 group-hover:translate-x-1 group-hover:-translate-y-1"
-              />
+              {isPending ? (
+                <Spinner />
+              ) : (
+                <div className="flex h-fit w-fit flex-row flex-nowrap items-center gap-1">
+                  Get Started
+                  <IconSend
+                    width={20}
+                    height={20}
+                    color="white"
+                    className="transiton-all duration-200 group-hover:translate-x-1 group-hover:-translate-y-1"
+                  />
+                </div>
+              )}
             </button>
           </NavbarButton>
         </form>
