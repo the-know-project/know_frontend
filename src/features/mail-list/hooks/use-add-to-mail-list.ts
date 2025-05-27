180d1d1863adbca807bf4f14e0c92799c6ea4a60
3illBaby <thrillanon@outlook.com>
2025-05-27 22:54:22 +0100

    feat(use-add-to-mail-list.tsx): impl hook

---
+++ src/features/mail-list/hooks/use-add-to-mail-list.ts
@@ -0,0 +1,32 @@
+import { useMutation } from "@tanstack/react-query";
+import { addToMailList } from "../api/route";
+import {
+  AddToMailListResponseDto,
+  IAddToMailList,
+} from "../schemas/email.schema";
+import { handleAxiosError } from "@/src/utils/handle-axios-error";
+
+export const useAddToMailList = () => {
+  return useMutation({
+    mutationFn: async (data: IAddToMailList) => {
+      try {
+        const result = await addToMailList(data);
+        return AddToMailListResponseDto.parse(result);
+      } catch (error) {
+        handleAxiosError(error);
+
+        throw new Error(
+          error instanceof Error
+            ? error.message
+            : "An unexpected error occurred",
+        );
+      }
+    },
+    onError: (error) => {
+      console.error("Failed to add to mail list:", error);
+    },
+    onSuccess: (data) => {
+      console.log("Successfully added to mail list:", data);
+    },
+  });
+};
