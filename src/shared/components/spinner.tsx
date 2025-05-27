8f400768d64c1d7a787935835196ffba67263e1c
3illBaby <thrillanon@outlook.com>
2025-05-27 23:33:41 +0100

    feat(spinner.tsx): added spinner

---
+++ src/shared/components/spinner.tsx
@@ -0,0 +1,7 @@
+const Spinner = () => {
+  return (
+    <div className="flex h-5 w-5 animate-spin items-center self-center rounded-full border-b-2 border-white accent-white"></div>
+  );
+};
+
+export default Spinner;
