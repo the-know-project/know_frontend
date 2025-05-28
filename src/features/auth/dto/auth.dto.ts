eee35e04f7a36f782f800b31189214113775e07f
3illBaby <thrillanon@outlook.com>
2025-05-28 22:49:46 +0100

    chore(auth.dto.ts): added response dto

--- src/features/auth/dto/auth.dto.ts
+++ src/features/auth/dto/auth.dto.ts
@@ -1,7 +1,20 @@
 import { z } from "zod";
 
+const AuthSuccessDataDto = z.object({
+  id: z.string().uuid(),
+  email: z.string().email().min(1).max(100),
+  refreshToken: z.string().uuid(),
+  accessToken: z.string().uuid(),
+});
+
 export const SignUpResponseDto = z.object({
   status: z.number(),
   message: z.string().min(1).max(100),
   data: z.string().min(1).max(100).optional(),
+});
+
+export const LoginResponseDto = z.object({
+  status: z.number(),
+  message: z.string().min(1).max(100),
+  data: AuthSuccessDataDto.optional(),
 });
