71197808d4da3bdb71640952ba08c15b29657743
3illBaby <thrillanon@outlook.com>
2025-05-28 22:54:48 +0100

    feat(route.ts): impl credential login route

--- src/features/auth/api/login/route.ts
+++ src/features/auth/api/login/route.ts
@@ -1,1 +1,20 @@
-export async function loginIn() {}
+import { isProduction } from "@/src/config/environment";
+import { env } from "@/src/config/schemas/env";
+import { createUrl } from "@/src/utils/url-factory";
+import axios from "axios";
+import { AUTH_OP } from "../../data/auth.path";
+import { ILogin } from "../../types/auth.types";
+
+function provideUrl() {
+  const config = env.env;
+  const baseUrl = isProduction() ? config.PROD_URL : config.STAGING_URL;
+  return createUrl({
+    baseUrl,
+    path: AUTH_OP.LOGIN,
+  });
+}
+
+export async function loginIn(ctx: ILogin) {
+  const response = await axios.post(provideUrl(), ctx);
+  return response.data;
+}
