5153150b00330bbcb19eb3faa3ae190c3a1de7f0
3illBaby <thrillanon@outlook.com>
2025-05-28 23:10:55 +0100

    chore(auth.types.ts): infer role type

--- src/features/auth/types/auth.types.ts
+++ src/features/auth/types/auth.types.ts
@@ -1,5 +1,6 @@
 import { z } from "zod";
-import { LoginSchema, SignUpSchema } from "../schema/auth.schema";
+import { LoginSchema, RoleSchema, SignUpSchema } from "../schema/auth.schema";
 
 export type ISignUp = z.infer<typeof SignUpSchema>;
 export type ILogin = z.infer<typeof LoginSchema>;
+export type IRole = z.infer<typeof RoleSchema>;
