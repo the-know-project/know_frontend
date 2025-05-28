754ea298e1214eb59e11e4fa6b6220f1d8dd7e90
3illBaby <thrillanon@outlook.com>
2025-05-28 21:49:39 +0100

    chore(auth.schema.ts): added sign up schema

---
+++ src/features/auth/schema/auth.schema.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const SignUpSchema = z.object({
+  firstName: z.string().min(2).max(100),
+  lastName: z.string().min(2).max(100),
+  userName: z.string().min(2).max(100),
+  email: z.string().email(),
+  password: z.string().min(8).max(100),
+  role: z.enum(["ARTIST", "BUYER"]),
+});
+
+// Types
