f8e419b3f72e7929f78ae90e16f4ea988ea5a74c
3illBaby <thrillanon@outlook.com>
2025-05-28 21:55:41 +0100

    feat(route.ts): impl sign up route

--- src/features/auth/api/sign-up/route.ts
+++ src/features/auth/api/sign-up/route.ts
@@ -1,1 +1,20 @@
-export async function signUp() {}
+import { isProduction } from "@/src/config/environment";
+import { env } from "@/src/config/schemas/env";
+import { createUrl } from "@/src/utils/url-factory";
+import axios from "axios";
+import { AUTH_OP } from "../../data/auth.path";
+import { ISignUp } from "../../types/auth.types";
+
+function provideUrl() {
+  const config = env.env;
+  const baseUrl = isProduction() ? config.PROD_URL : config.STAGING_URL;
+  return createUrl({
+    baseUrl,
+    path: AUTH_OP.SIGN_UP,
+  });
+}
+
+export async function signUp(ctx: ISignUp) {
+  const response = await axios.post(provideUrl(), ctx);
+  return response.data;
+}
