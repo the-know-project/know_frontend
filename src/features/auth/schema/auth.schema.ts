28a327afb381485496ffe683c40c502cd5757b71
3illBaby <thrillanon@outlook.com>
2025-05-28 23:09:56 +0100

    refactor(auth.schema.ts): standalone role schema

--- src/features/auth/schema/auth.schema.ts
+++ src/features/auth/schema/auth.schema.ts
@@ -1,12 +1,14 @@
 import { z } from "zod";
 
+export const RoleSchema = z.enum(["ARTIST", "BUYER"]);
+
 export const SignUpSchema = z.object({
   firstName: z.string().min(2).max(100),
   lastName: z.string().min(2).max(100),
   userName: z.string().min(2).max(100),
   email: z.string().email(),
   password: z.string().min(8).max(100),
-  role: z.enum(["ARTIST", "BUYER"]),
+  role: RoleSchema,
 });
 
 export const LoginSchema = z.object({
