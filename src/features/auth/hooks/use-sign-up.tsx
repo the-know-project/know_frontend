b72e85b697ccb033cfe763ab49e0536b7bcb0022
3illBaby <thrillanon@outlook.com>
2025-05-28 23:52:51 +0100

    refactor(use-sign-up.tsx): fetch role from state

--- src/features/auth/hooks/use-sign-up.tsx
+++ src/features/auth/hooks/use-sign-up.tsx
@@ -1,15 +1,20 @@
-import { useMutation } from "@tanstack/react-query";
-import { ISignUp } from "../types/auth.types";
 import { handleAxiosError } from "@/src/utils/handle-axios-error";
+import { useMutation } from "@tanstack/react-query";
 import Router from "next/router";
 import { signUp } from "../api/sign-up/route";
 import { SignUpResponseDto } from "../dto/auth.dto";
+import { useRoleStore } from "../state/store/role.store";
+import { ISignUp } from "../types/auth.types";
 
 export const useSignUp = () => {
+  const role = useRoleStore((state) => state.role);
   return useMutation({
-    mutationFn: async (ctx: ISignUp) => {
+    mutationFn: async (ctx: Omit<ISignUp, "role">) => {
       try {
-        const result = await signUp(ctx);
+        const result = await signUp({
+          ...ctx,
+          role: role,
+        });
         return SignUpResponseDto.parse(result);
       } catch (error) {
         handleAxiosError(error);
