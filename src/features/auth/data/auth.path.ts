0ce40ab7d465b0cee9195613c70c8c1a1a68a168
3illBaby <thrillanon@outlook.com>
2025-05-28 21:31:35 +0100

    chore(auth.path.ts): added path data

---
+++ src/features/auth/data/auth.path.ts
@@ -0,0 +1,7 @@
+export enum AuthPath {
+  LOGIN = "api/auth/login",
+  LOGOUT = "api/auth/logout",
+  SIGN_UP = "api/auth/registerUser",
+  GOOGLE_LOGIN = "api/auth/google-login",
+  DISCORD_LOGIN = "api/auth/discord-login",
+}
