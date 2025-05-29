0539f1e357af434c0a5c9ebd054b29a7d6fb6fe5
3illBaby <thrillanon@outlook.com>
2025-05-29 23:33:06 +0100

    refactor(use-login.tsx): enhance login and update token state

--- src/features/auth/hooks/use-login.tsx
+++ src/features/auth/hooks/use-login.tsx
@@ -1,15 +1,40 @@
 import { useMutation } from "@tanstack/react-query";
-import { ILogin } from "../types/auth.types";
+import { ILogin, IRole } from "../types/auth.types";
 import { handleAxiosError } from "@/src/utils/handle-axios-error";
 import { login } from "../api/login/route";
 import { LoginResponseDto } from "../dto/auth.dto";
+import { useAuth } from "./use-auth";
+
+interface LoginSuccess {
+  user: {
+    id: string;
+    email: string;
+  };
+  tokens: {
+    accessToken: string;
+    refreshToken: string;
+  };
+  role?: IRole;
+}
 
 export const useLogin = () => {
+  const auth = useAuth();
+
   return useMutation({
-    mutationFn: async (ctx: ILogin) => {
+    mutationFn: async (ctx: ILogin): Promise<LoginSuccess> => {
       try {
         const data = await login(ctx);
-        return LoginResponseDto.parse(data);
+        const validatedData = LoginResponseDto.parse(data);
+        if (validatedData.status !== 200 || !validatedData.data) {
+          throw new Error(validatedData.message || "Login Failed");
+        }
+
+        const { id, email, accessToken, refreshToken } = validatedData.data;
+
+        return {
+          user: { id, email },
+          tokens: { accessToken, refreshToken },
+        };
       } catch (error) {
         handleAxiosError(error);
 
@@ -19,6 +44,20 @@ export const useLogin = () => {
             : "An unexpected error occurred",
         );
       }
+    },
+
+    onSuccess: (data, variables) => {
+      auth.login(
+        data.tokens.accessToken,
+        data.tokens.refreshToken,
+        data.user,
+        data.role || "NONE",
+      );
+      console.log(`Login Successful : ${data.user}`);
+    },
+    onError: (error) => {
+      auth.logout();
+      console.error(`Login failed ${error}`);
     },
   });
 };
