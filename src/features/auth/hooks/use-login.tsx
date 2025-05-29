ecc2b66513026f9509c123179dcf857aec2bc6c7
3illBaby <thrillanon@outlook.com>
2025-05-29 16:51:04 +0100

    feat(use-login.tsx): consumed login func

--- src/features/auth/hooks/use-login.tsx
+++ src/features/auth/hooks/use-login.tsx
@@ -1,11 +1,15 @@
 import { useMutation } from "@tanstack/react-query";
 import { ILogin } from "../types/auth.types";
 import { handleAxiosError } from "@/src/utils/handle-axios-error";
+import { login } from "../api/login/route";
+import { LoginResponseDto } from "../dto/auth.dto";
 
 export const useLogin = () => {
   return useMutation({
     mutationFn: async (ctx: ILogin) => {
       try {
+        const data = await login(ctx);
+        return LoginResponseDto.parse(data);
       } catch (error) {
         handleAxiosError(error);
 
