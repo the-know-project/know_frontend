337f1f70449f47b333c7a3b219deb5d22d2290f9
3illBaby <thrillanon@outlook.com>
2025-05-29 14:09:08 +0100

    style(page.ts): added grid background
    made this a correct page

--- src/app/(auth)/role/page.tsx
+++ src/app/(auth)/role/page.tsx
@@ -1,7 +1,16 @@
+import GridBackground from "@/src/shared/components/grid-background";
 import SelectRole from "@/src/shared/components/role-selection";
 
 const Page = () => {
-  return <SelectRole />;
+  return (
+    <section className="flex w-full flex-col scroll-smooth">
+      <GridBackground>
+        <>
+          <SelectRole />
+        </>
+      </GridBackground>
+    </section>
+  );
 };
 
 export default Page;
