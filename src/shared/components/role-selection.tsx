692f8c46640f0ac191bfed4248243e86a4140d73
3illBaby <thrillanon@outlook.com>
2025-05-29 14:07:34 +0100

    perf(role-selection.tsx): improved component performance and style

--- src/shared/components/role-selection.tsx
+++ src/shared/components/role-selection.tsx
@@ -1,75 +1,90 @@
 "use client";
 
 import Image from "next/image";
-import { LuUserRound } from "react-icons/lu";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { RoleSelectionItems } from "@/src/constants/constants";
+import { TitleText } from "../layout/header";
+import { useRoleStore } from "@/src/features/auth/state/store/role.store";
 
 export default function SelectRole() {
   const router = useRouter();
+  const selectRole = useRoleStore((state) => state.setRole);
 
   const handleSelectRole = (selectedRole: string) => {
-    console.log(selectedRole);
-    router.push("/auth/signup");
+    if (selectedRole === "Artist") {
+      selectRole("ARTIST");
+    } else if (selectedRole === "Buyer") {
+      selectRole("BUYER");
+    }
+
+    router.push("/signup");
   };
 
   return (
-    <div className="grid min-h-screen grid-cols-1 md:grid-cols-2">
-      <div className="flex flex-col justify-center bg-white px-8 py-12 md:px-16">
+    <div className="relative z-50 grid min-h-screen grid-cols-1 md:grid-cols-2">
+      <div className="flex flex-col justify-center bg-transparent px-8 py-12 md:px-16">
         <div className="mb-10 flex items-center justify-between">
-          <Image src="/Know-Logo.png" alt="Logo" width={60} height={60} />
-          <p className="text-sm text-neutral-500">
+          <Image
+            src="/Know-Logo.png"
+            alt="Logo"
+            width={60}
+            height={60}
+            quality={100}
+          />
+          <p className="font-bricolage text-sm font-medium text-neutral-600">
             Already have an account?{" "}
-            <a href="/login" className="text-blue-600 hover:underline">
+            <Link
+              href="/login"
+              className="font-semibold text-blue-600 transition-colors hover:text-blue-700 hover:underline"
+            >
               Log In
-            </a>
+            </Link>
           </p>
         </div>
 
-        <h1 className="mb-2 text-3xl font-semibold text-black">Sign Up</h1>
-        <p className="mb-8 text-sm text-neutral-500">
-          To get started, please select your primary role. You can switch
-          between roles at any time in your profile.
-        </p>
+        <div className="mb-8 flex max-w-prose flex-col gap-2">
+          <TitleText textStyles={`w-full`}>
+            <h1 className="font-helvetica text-3xl font-semibold text-black">
+              Sign Up
+            </h1>
+          </TitleText>
+          <p className="font-bricolage motion-preset-expand motion-duration-700 text-sm text-neutral-500">
+            To get started, please select your primary role. You can switch
+            between roles at any time in your profile.
+          </p>
+        </div>
 
-        <div className="space-y-4">
-          <Link href={"/auth/signup"}>
-            <button
-              onClick={() => handleSelectRole("ARTIST")}
-              className="flex w-full cursor-pointer items-start gap-3 rounded-lg border border-b-neutral-300 p-4"
+        <div className="flex w-full flex-col space-y-4">
+          {RoleSelectionItems.map((item) => (
+            <div
+              key={item.id}
+              className={`flex w-full flex-col ${item.name === "Artist" ? "animated-purple-glow motion-preset-expand motion-duration-700" : "animated-yellow-glow motion-preset-expand motion-duration-700 motion-delay-700"}`}
             >
-              <div className="flex h-[20px] w-[20px] items-center justify-center rounded-[24px] bg-gray-400/35 md:h-[52px] md:w-[52px]">
-                <LuUserRound className="mt-1 text-2xl text-neutral-700 md:text-3xl" />
-              </div>
-
-              <div className="text-left">
-                <p className="text-lg font-medium text-black">Artist</p>
-                <p className="text-sm text-neutral-500">
-                  Showcase your art to a global audience and expand your reach.
-                </p>
-              </div>
-            </button>
-          </Link>
-
-          <button
-            onClick={() => handleSelectRole("BUYER")}
-            className="flex w-full cursor-pointer items-start gap-3 rounded-lg p-4 transition hover:border-black"
-          >
-            <div className="flex h-[20px] w-[20px] items-center justify-center rounded-[24px] bg-gray-400/35 md:h-[52px] md:w-[52px]">
-              <LuUserRound className="mt-1 text-2xl text-neutral-700 md:text-3xl" />
+              <button
+                onClick={() => handleSelectRole(item.name)}
+                className={`flex w-full flex-row items-center gap-2 rounded-[15px] bg-transparent p-2 shadow-md hover:scale-105 active:scale-95`}
+              >
+                <div
+                  className={`flex w-fit rounded-full p-2 ${item.name === "Artist" ? "bg-purple-600" : "bg-yellow-500"}`}
+                >
+                  {item.icon}
+                </div>
+                <div className="flex flex-col gap-2 text-start">
+                  <h3 className="font-helvetica text-lg font-medium text-black">
+                    {item.name}
+                  </h3>
+                  <p className="font-bricolage text-sm text-neutral-500">
+                    {item.content}
+                  </p>
+                </div>
+              </button>
             </div>
-            <div className="text-left">
-              <p className="text-lg font-medium text-black">Buyer</p>
-              <p className="text-sm text-neutral-500">
-                Discover unique artworks, connect with artists, and build your
-                collection.
-              </p>
-            </div>
-          </button>
+          ))}
         </div>
       </div>
 
-      <div className="relative hidden md:block">
+      <div className="motion-preset-blur-right-lg motion-duration-700 relative hidden md:block">
         <Image
           src="/roleselect.png"
           alt="Signup Visual"
