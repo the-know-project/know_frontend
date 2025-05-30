a4adcd02da7e92479ad41c163f92b485626f6cc6
3illBaby <thrillanon@outlook.com>
2025-05-31 00:14:33 +0100

    fix(auth.path.ts): fixed incorrect auth path

--- src/features/auth/data/auth.path.ts
+++ src/features/auth/data/auth.path.ts
@@ -1,8 +1,8 @@
 export enum AUTH_OP {
-  LOGIN = "api/auth/login",
-  LOGOUT = "api/auth/logout",
-  SIGN_UP = "api/auth/registerUser",
-  GOOGLE_LOGIN = "api/auth/google-login",
-  DISCORD_LOGIN = "api/auth/discord-login",
-  REFRESH_TOKEN = "api/token/refreshToken",
+  LOGIN = "/api/auth/login",
+  LOGOUT = "/api/auth/logout",
+  SIGN_UP = "/api/auth/registerUser",
+  GOOGLE_LOGIN = "/api/auth/google-login",
+  DISCORD_LOGIN = "/api/auth/discord-login",
+  REFRESH_TOKEN = "/api/token/refreshToken",
 }
