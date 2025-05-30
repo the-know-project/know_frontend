da6f0e852b5fa3798400d66792651a99923fc7ad
3illBaby <thrillanon@outlook.com>
2025-05-30 23:19:05 +0100

    chore(auth.types.ts): created signup type

--- src/features/auth/types/auth.types.ts
+++ src/features/auth/types/auth.types.ts
@@ -1,6 +1,12 @@
 import { z } from "zod";
-import { LoginSchema, RoleSchema, SignUpSchema } from "../schema/auth.schema";
+import {
+  LoginSchema,
+  RoleSchema,
+  SignUpFormSchema,
+  SignUpSchema,
+} from "../schema/auth.schema";
 
 export type ISignUp = z.infer<typeof SignUpSchema>;
 export type ILogin = z.infer<typeof LoginSchema>;
 export type IRole = z.infer<typeof RoleSchema>;
+export type ISignUpForm = z.infer<typeof SignUpFormSchema>;
