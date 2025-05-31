9e3cb86c7d0198a8fcf4a7d480486dc4299e0db7
3illBaby <thrillanon@outlook.com>
2025-05-31 14:39:34 +0100

    refactor(auth.schema.ts): extended login schema

--- src/features/auth/schema/auth.schema.ts
+++ src/features/auth/schema/auth.schema.ts
@@ -21,5 +21,5 @@ export const SignUpFormSchema = z.object({
 
 export const LoginSchema = z.object({
   email: z.string().email(),
-  password: z.string().min(8).max(100),
+  password: z.string().min(6, "at least 6 characters").max(100),
 });
