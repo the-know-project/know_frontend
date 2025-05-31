89d66b2cd45c2064179514f3f0f71f1c96b24cec
3illBaby <thrillanon@outlook.com>
2025-05-31 20:18:32 +0100

    chore(auth.types.ts): added login types

--- src/features/auth/types/auth.types.ts
+++ src/features/auth/types/auth.types.ts
@@ -5,8 +5,24 @@ import {
   SignUpFormSchema,
   SignUpSchema,
 } from "../schema/auth.schema";
+import { SignUpResponseDto } from "../dto/auth.dto";
+
+export interface ILoginSuccess {
+  status: number;
+  message: string;
+  user: {
+    id: string;
+    email: string;
+  };
+  tokens: {
+    accessToken: string;
+    refreshToken: string;
+  };
+  role?: IRole;
+}
 
 export type ISignUp = z.infer<typeof SignUpSchema>;
 export type ILogin = z.infer<typeof LoginSchema>;
 export type IRole = z.infer<typeof RoleSchema>;
 export type ISignUpForm = z.infer<typeof SignUpFormSchema>;
+export type ISignUpResponseDto = z.infer<typeof SignUpResponseDto>;
