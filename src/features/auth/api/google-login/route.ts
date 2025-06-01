f18db4c0a3cd77a76150f87e4ddf350166bdf367
3illBaby <thrillanon@outlook.com>
2025-06-01 21:00:14 +0100

    fix(route.ts): fixed cors issue

--- src/features/auth/api/google-login/route.ts
+++ src/features/auth/api/google-login/route.ts
@@ -1,6 +1,6 @@
 import { AUTH_OP } from "../../data/auth.path";
 import { AuthenticatedApiClient } from "../http/authenticated-client";
 
-export async function googleLogin() {
-  return await AuthenticatedApiClient.get(AUTH_OP.GOOGLE_LOGIN);
+export function googleLogin() {
+  return AuthenticatedApiClient.window(AUTH_OP.GOOGLE_LOGIN);
 }
