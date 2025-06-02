f55ba7f267fbc92a8d6a2c3ab444bf2b00828068
3illBaby <thrillanon@outlook.com>
2025-06-03 00:24:35 +0100

    chore(personalize.path.ts): added api path

---
+++ src/features/personalize/data/personalize.path.ts
@@ -0,0 +1,5 @@
+export enum PERSONALIZE_OP {
+  PERSONALIZE_EXP = "/api/user/perosonalizeExperience",
+  GET_CATEGORIES = "/api/categories/getCategories",
+  ADD_CATEGORY = "/api/categories/addCategories",
+}
