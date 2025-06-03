b296be1b9b6bb5d64a15fb5efb89368c3acd0e4d
3illBaby <thrillanon@outlook.com>
2025-06-03 10:01:13 +0100

    Simplify art preferences from objects to strings

--- src/features/personalize/data/personalize.data.ts
+++ src/features/personalize/data/personalize.data.ts
@@ -1,51 +1,13 @@
 export const DummyArtPreferences = [
-  {
-    id: 1,
-    name: "Abstract",
-  },
-  {
-    id: 2,
-    name: "Realistic",
-  },
-  {
-    id: 3,
-    name: "Cartoon",
-  },
-  {
-    id: 4,
-    name: "Expression",
-  },
-  {
-    id: 5,
-    name: "Pop Art",
-  },
-  {
-    id: 6,
-    name: "Minimalist",
-  },
-  {
-    id: 7,
-    name: "Anime",
-  },
-
-  {
-    id: 8,
-    name: "Impressionist",
-  },
-  {
-    id: 9,
-    name: "Surrealism",
-  },
-  {
-    id: 10,
-    name: "Cubism",
-  },
-  {
-    id: 11,
-    name: "Futurism",
-  },
-  {
-    id: 12,
-    name: "Photography",
-  },
+  "abstract",
+  "realistic",
+  "cartoon",
+  "anime",
+  "expression",
+  "pop art",
+  "minimalist",
+  "impressionist",
+  "cubism",
+  "futurism",
+  "photography",
 ];
