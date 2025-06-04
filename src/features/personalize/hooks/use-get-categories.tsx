73899c11218cc9fa4687c88fbb60396590e1f73a
3illBaby <thrillanon@outlook.com>
2025-06-04 21:12:01 +0100

    refactor(use-get-categories.tsx): consolidate hook logic for easy prefetching refrence

--- src/features/personalize/hooks/use-get-categories.tsx
+++ src/features/personalize/hooks/use-get-categories.tsx
@@ -1,25 +1,6 @@
-import { handleAxiosError } from "@/src/utils/handle-axios-error";
 import { useQuery } from "@tanstack/react-query";
-import { getCategories } from "../api/categories/get-categories/route";
-import { GetCategoriesResponse } from "../dto/personalize.dto";
+import { getCategoriesQueryOptions } from "../queries/get-categories.queries";
 
 export const useGetCategories = () => {
-  return useQuery({
-    queryKey: ["get-categories"],
-    queryFn: async () => {
-      try {
-        const result = await getCategories();
-        const data = GetCategoriesResponse.parse(result);
-        return data.data;
-      } catch (error) {
-        handleAxiosError(error);
-
-        throw new Error(
-          error instanceof Error
-            ? error.message
-            : "An unexpected error occurred",
-        );
-      }
-    },
-  });
+  return useQuery(getCategoriesQueryOptions);
 };
