f0db4e147462a7e9c9a4131c0801e450923c2afc
3illBaby <thrillanon@outlook.com>
2025-06-05 12:28:10 +0100

    feat(art-selection.tsx): integrate personalize exp

--- src/features/personalize/components/art-selection.tsx
+++ src/features/personalize/components/art-selection.tsx
@@ -1,16 +1,28 @@
 "use client";
 
-import { NavbarButton } from "@/src/shared/ui/resizable-navbar";
-import { DummyArtPreferences } from "../data/personalize.data";
-import { IconArrowRight } from "@tabler/icons-react";
-import { useState } from "react";
 import { cn } from "@/lib/utils";
-import { useGetCategories } from "../hooks/use-get-categories";
+import Spinner from "@/src/shared/components/spinner";
+import ToastDescription from "@/src/shared/components/toast-description";
+import ToastIcon from "@/src/shared/components/toast-icon";
+import { NavbarButton } from "@/src/shared/ui/resizable-navbar";
+import { IconArrowRight } from "@tabler/icons-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { toast } from "sonner";
+import { DummyArtPreferences } from "../data/personalize.data";
+import { useGetCategories, usePersonalizeExp } from "../hooks";
 import ArtSelectionSkeleton from "./art-selection-skeleton";
+
+interface IPersonalizeExp {
+  status: number;
+  message: string;
+}
 
 const ArtSelection = () => {
   const { data, isLoading, error } = useGetCategories();
+  const { mutateAsync: personalizeExp, isPending } = usePersonalizeExp();
   const [selectedArt, setSelectedArt] = useState<string[]>([]);
+  const router = useRouter();
 
   const handleSelection = (pref: string) => {
     setSelectedArt((prev) => {
@@ -23,6 +35,57 @@ const ArtSelection = () => {
   };
 
   const isItemSelected = (item: string) => selectedArt.includes(item);
+
+  const handleToast = (data: IPersonalizeExp) => {
+    if (data.status === 200) {
+      toast("", {
+        icon: <ToastIcon />,
+        description: <ToastDescription description={data.message} />,
+        style: {
+          backdropFilter: "-moz-initial",
+          opacity: "-moz-initial",
+          backgroundColor: " oklch(62.7% 0.194 149.214)",
+          fontSize: "15px",
+          font: "Space Grotesk",
+          color: "#ffffff",
+          fontWeight: "bolder",
+        },
+      });
+      router.push("/explore");
+    } else {
+      toast("", {
+        icon: <ToastIcon />,
+        description: <ToastDescription description={`An error occurred`} />,
+        style: {
+          backdropFilter: "-moz-initial",
+          opacity: "-moz-initial",
+          backgroundColor: "oklch(62.8% 0.258 29.234)",
+          fontSize: "15px",
+          font: "Space Grotesk",
+          color: "#ffffff",
+          fontWeight: "bolder",
+        },
+      });
+    }
+
+    toast("", {
+      icon: <ToastIcon />,
+      description: <ToastDescription description={`An error occurred`} />,
+      style: {
+        backdropFilter: "-moz-initial",
+        opacity: "-moz-initial",
+        backgroundColor: "oklch(62.8% 0.258 29.234)",
+        fontSize: "15px",
+        font: "Space Grotesk",
+        color: "#ffffff",
+        fontWeight: "bolder",
+      },
+    });
+  };
+
+  const handlePersonalizeExp = async (ctx: string[]) => {
+    const data = await personalizeExp(ctx);
+  };
 
   if (isLoading) {
     return <ArtSelectionSkeleton />;
@@ -63,8 +126,16 @@ const ArtSelection = () => {
         className="mt-[150px] flex w-full items-center justify-center self-center"
       >
         <button className="font-bebas group relative inline-flex w-full items-center justify-center gap-1 self-center rounded-lg bg-black px-2.5 py-1.5 text-[16px] font-medium text-nowrap text-white capitalize outline outline-[#fff2f21f] transition-all duration-300 hover:scale-110 active:scale-95">
-          Get Started{" "}
-          <IconArrowRight width={20} height={20} className="button_icon" />
+          {isPending ? (
+            <div className="flex items-center justify-center">
+              <Spinner borderColor="border-white" />
+            </div>
+          ) : (
+            <>
+              Get Started{" "}
+              <IconArrowRight width={20} height={20} className="button_icon" />
+            </>
+          )}
         </button>
       </NavbarButton>
     </section>
