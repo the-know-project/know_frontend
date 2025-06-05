5692d8ba0f1ebd97d6de20c749dd60fa233b5e15
3illBaby <thrillanon@outlook.com>
2025-06-06 00:47:56 +0100

    Add categories section to explore page

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,11 +1,30 @@
 import { PageAuthGuard } from "@/src/features/auth/guards";
+import ExploreCategories from "@/src/features/explore/components/explore-categories";
+import { getCategoriesQueryOptions } from "@/src/features/personalize/queries/get-categories.queries";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 
-const Explore = () => {
+const Page = async () => {
+  const queryClient = new QueryClient();
+  try {
+    await queryClient.prefetchQuery(getCategoriesQueryOptions);
+  } catch (error) {
+    return <p>Failed to load preferences: {(error as Error).message}</p>;
+  }
   return (
     <PageAuthGuard requiresAuth>
-      <div>Explore page</div>
+      <section className="relative z-50 flex w-full px-6">
+        <div className="mt-2 flex w-full">
+          <HydrationBoundary state={dehydrate(queryClient)}>
+            <ExploreCategories />
+          </HydrationBoundary>
+        </div>
+      </section>
     </PageAuthGuard>
   );
 };
 
-export default Explore;
+export default Page;
