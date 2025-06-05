0602d4034650530aa798c05b877f30d8635618eb
3illBaby <thrillanon@outlook.com>
2025-06-06 00:49:50 +0100

    chore(layout.tsx): created auth layout

---
+++ src/app/explore/layout.tsx
@@ -0,0 +1,21 @@
+import ExploreNav from "@/src/features/explore/components/explore-nav";
+import GridBackground from "@/src/shared/components/grid-background";
+
+interface ILayout {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<ILayout> = ({ children }) => {
+  return (
+    <section className="flex w-full flex-col">
+      <GridBackground>
+        <section className="flex min-h-screen w-full flex-col">
+          <ExploreNav />
+          {children}
+        </section>
+      </GridBackground>
+    </section>
+  );
+};
+
+export default Layout;
