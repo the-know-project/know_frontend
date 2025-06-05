d6cea24c4b564c00747c4824a7a5f363cb32e797
3illBaby <thrillanon@outlook.com>
2025-06-05 13:18:43 +0100

    fix(personalize.path.ts): fixed error in path

--- src/features/personalize/data/personalize.path.ts
+++ src/features/personalize/data/personalize.path.ts
@@ -1,5 +1,5 @@
 export enum PERSONALIZE_OP {
-  PERSONALIZE_EXP = "/api/user/perosonalizeExperience",
+  PERSONALIZE_EXP = "/api/user/personalizeExperience",
   GET_CATEGORIES = "/api/categories/getCategories",
   ADD_CATEGORY = "/api/categories/addCategories",
 }
