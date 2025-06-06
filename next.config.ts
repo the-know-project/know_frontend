677665665464de41707948b067d2efbce942d8fb
3illBaby <thrillanon@outlook.com>
2025-06-06 16:49:21 +0100

    chore(next.config.ts): added image config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.kravemarketingllc.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "google.com",
+        port: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
