a3134b191ccd3ac86a7f9c3665843c98b84b0196
3illBaby <thrillanon@outlook.com>
2025-06-06 16:53:14 +0100

    chore(explore-canvas.tsx): created explore canvas

---
+++ src/features/explore/components/explore-canvas.tsx
@@ -0,0 +1,9 @@
+const ExploreCanvas = () => {
+  return (
+    <section className="flex w-full flex-col">
+      <div className="grid min-h-screen grid-cols-1 md:grid-cols-2 lg:grid-cols-3"></div>
+    </section>
+  );
+};
+
+export default ExploreCanvas;
