8e50be908763dda7c060c18b8c0b1c19efb1d846
3illBaby <thrillanon@outlook.com>
2025-06-06 08:46:25 +0100

    style(explore-nav.tsx): incrsd font size

--- src/features/explore/components/explore-nav.tsx
+++ src/features/explore/components/explore-nav.tsx
@@ -43,21 +43,15 @@ const ExploreNav = () => {
             colors={["#FF5733", "#33FF57", "#3357FF", "#F1C40F"]}
             className="w-fit"
           >
-            <button className="font-bebas relative inline-flex w-fit items-center gap-1 rounded-lg bg-zinc-950 px-2.5 py-1.5 text-[12px] font-medium text-white capitalize outline outline-[#fff2f21f] transition-all duration-200 sm:text-[16px]">
+            <button className="font-bebas relative inline-flex w-fit items-center gap-1 rounded-lg bg-zinc-950 px-2.5 py-1.5 text-sm font-medium text-white capitalize outline outline-[#fff2f21f] transition-all duration-200 sm:text-[16px]">
               <p className="block">share your work</p>
             </button>
           </NavbarButton>
 
           <div className="explore_nav_wrapper">
             <div className="flex items-center gap-2 bg-transparent px-2">
-              <IconNotification
-                color="black"
-                className="h-[20px] w-[20px] sm:h-[30px] sm:w-[30px]"
-              />
-              <IconUser
-                color="black"
-                className="h-[20px] w-[20px] sm:h-[30px] sm:w-[30px]"
-              />
+              <IconNotification color="black" className="h-[30px] w-[30px]" />
+              <IconUser color="black" className="h-[30px] w-[30px]" />
             </div>
           </div>
         </div>
