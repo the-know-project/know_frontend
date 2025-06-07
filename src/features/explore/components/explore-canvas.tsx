8bcc7dbf004b46e0d18d51cbd8a095c51a799468
3illBaby <thrillanon@outlook.com>
2025-06-07 12:12:44 +0100

    feat(explore-canvas.tsx): impl explore canvas

--- src/features/explore/components/explore-canvas.tsx
+++ src/features/explore/components/explore-canvas.tsx
@@ -1,7 +1,29 @@
+import { ExploreDemoItem } from "@/src/constants/constants";
+import ExploreCard from "./explore-card";
+
 const ExploreCanvas = () => {
   return (
-    <section className="flex w-full flex-col">
-      <div className="grid min-h-screen grid-cols-1 md:grid-cols-2 lg:grid-cols-3"></div>
+    <section className="flex w-full flex-col items-center justify-center">
+      <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
+        {ExploreDemoItem.map((item, index) => (
+          <div
+            key={item.id}
+            className="motion-preset-expand motion-duration-700"
+            style={{
+              animationDelay: `${index * 100}ms`,
+            }}
+          >
+            <ExploreCard
+              id={item.id}
+              artWork={item.artWork}
+              artName={item.artName}
+              artistImage={item.artistImage}
+              artistName={item.artistName}
+              likeCount={item.likeCount}
+            />
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
