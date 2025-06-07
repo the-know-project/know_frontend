3698409e6e1f60ac19480ee8ffbd488eaaf13e00
3illBaby <thrillanon@outlook.com>
2025-06-07 12:55:59 +0100

    fix(next.config.ts): fixed wrong google image hostname

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "google.com",
+        hostname: "lh3.googleusercontent.com",
         port: "",
       },
     ],
