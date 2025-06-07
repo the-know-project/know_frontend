97d0fa5ea44bdc2a45bb9e48a7d0f42c5a8681da
3illBaby <thrillanon@outlook.com>
2025-06-07 15:29:33 +0100

    Show share work button only to artist users

--- src/features/explore/components/explore-nav.tsx
+++ src/features/explore/components/explore-nav.tsx
@@ -5,9 +5,15 @@ import { IconNotification, IconUser } from "@tabler/icons-react";
 import Image from "next/image";
 import ExploreForm from "./explore-form";
 import { useTokenStore } from "../../auth/state/store";
+import { useRouter } from "next/navigation";
 
 const ExploreNav = () => {
+  const router = useRouter();
   const user = useTokenStore((state) => state.user);
+
+  const handleShareWork = () => {
+    router.push("/upload");
+  };
 
   return (
     <nav className="motion-preset-expand motion-duration-700 relative z-50 flex w-full flex-col gap-1 py-1">
@@ -42,14 +48,19 @@ const ExploreNav = () => {
         </div>
 
         <div className="flex items-center gap-5">
-          <NavbarButton
-            colors={["#FF5733", "#33FF57", "#3357FF", "#F1C40F"]}
-            className="w-fit"
-          >
-            <button className="font-bebas relative inline-flex w-fit items-center gap-1 rounded-lg bg-zinc-950 px-2.5 py-1.5 text-sm font-medium text-white capitalize outline outline-[#fff2f21f] transition-all duration-200 sm:text-[16px]">
-              <p className="block">share your work</p>
-            </button>
-          </NavbarButton>
+          {user && user?.role === "ARTIST" && (
+            <NavbarButton
+              colors={["#FF5733", "#33FF57", "#3357FF", "#F1C40F"]}
+              className="w-fit"
+            >
+              <button
+                className="font-bebas relative inline-flex w-fit items-center gap-1 rounded-lg bg-zinc-950 px-2.5 py-1.5 text-sm font-medium text-white capitalize outline outline-[#fff2f21f] transition-all duration-200 sm:text-[16px]"
+                onClick={handleShareWork}
+              >
+                <p className="block">share your work</p>
+              </button>
+            </NavbarButton>
+          )}
 
           <div className="explore_nav_wrapper">
             <div className="flex items-center gap-2 bg-transparent px-2">
