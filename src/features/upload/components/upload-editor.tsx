8f768ae0d333fa965105e956b87fee3a881ee847
3illBaby <thrillanon@outlook.com>
2025-06-09 23:36:55 +0100

    Update heading styles and text in upload editor

--- src/features/upload/components/upload-editor.tsx
+++ src/features/upload/components/upload-editor.tsx
@@ -3,27 +3,26 @@ import TagPickerForm from "./tag-picker.form";
 import CategoryPickerForm from "./category-picker.form";
 
 const UploadEditor = () => {
-  
   return (
     <section className="flex w-full flex-col gap-[100px]">
       {/* Size Editor */}
       <div className="flex flex-col items-start gap-3">
-        <h3 className="font-bebas text-lg tracking-wider text-neutral-900">
-          size
+        <h3 className="font-bebas text-xl tracking-wider text-black capitalize">
+          edit size
         </h3>
         <SizePickerForm />
       </div>
 
       <div className="flex flex-col items-start gap-3">
-        <h3 className="font-bebas text-lg tracking-wider text-neutral-900">
-          Tags
+        <h3 className="font-bebas text-xl tracking-wider text-neutral-900">
+          Add Tags
         </h3>
         <TagPickerForm />
       </div>
 
       <div className="flex flex-col items-start gap-3">
         <h3 className="font-bebas text-lg tracking-wider text-neutral-900">
-          Art Category
+          Choose Category
         </h3>
         <CategoryPickerForm />
       </div>
