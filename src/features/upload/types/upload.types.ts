ce2b119a2836d015faf84da3c4b27fcda99860f9
3illBaby <thrillanon@outlook.com>
2025-06-10 21:40:28 +0100

    chore(upload.types.ts): added types

--- src/features/upload/types/upload.types.ts
+++ src/features/upload/types/upload.types.ts
@@ -1,6 +1,12 @@
 import { z } from "zod";
-import { SizePickerSchema, UploadSchema, UploadFormSchema } from "../schema/upload.schema";
+import {
+  SizePickerSchema,
+  UploadSchema,
+  UploadFormSchema,
+  UploadAssetSchema,
+} from "../schema/upload.schema";
 
 export type IUploadForm = z.infer<typeof UploadSchema>;
 export type IUploadFormState = z.infer<typeof UploadFormSchema>;
 export type ISizePickerForm = z.infer<typeof SizePickerSchema>;
+export type IUploadAsset = z.infer<typeof UploadAssetSchema>;
