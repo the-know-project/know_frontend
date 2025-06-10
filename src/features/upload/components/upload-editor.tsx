a1c1cbe20367544e695f1a7719e34711f100b9f5
3illBaby <thrillanon@outlook.com>
2025-06-10 21:11:23 +0100

    Extract editor title styles into reusable class

--- src/features/upload/components/upload-editor.tsx
+++ src/features/upload/components/upload-editor.tsx
@@ -7,23 +7,17 @@ const UploadEditor = () => {
     <section className="flex w-full flex-col gap-[100px]">
       {/* Size Editor */}
       <div className="flex flex-col items-start gap-3">
-        <h3 className="font-bebas text-xl tracking-wider text-black capitalize">
-          edit size
-        </h3>
+        <h3 className="editor_title_font">edit size</h3>
         <SizePickerForm />
       </div>
 
       <div className="flex flex-col items-start gap-3">
-        <h3 className="font-bebas text-xl tracking-wider text-neutral-900">
-          Add Tags
-        </h3>
+        <h3 className="editor_title_font">Add Tags</h3>
         <TagPickerForm />
       </div>
 
       <div className="flex flex-col items-start gap-3">
-        <h3 className="font-bebas text-lg tracking-wider text-neutral-900">
-          Choose Category
-        </h3>
+        <h3 className="editor_title_font">Choose Category</h3>
         <CategoryPickerForm />
       </div>
     </section>
