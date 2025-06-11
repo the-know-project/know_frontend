825822d68699e1ac4eee5939bf6f499a55e61f92
3illBaby <thrillanon@outlook.com>
2025-06-11 22:40:42 +0100

    feat(use-fetch-explore-asset.tsx): impl hook to fetch explore assets

--- src/features/explore/hooks/use-fetch-explore-asset.tsx
+++ src/features/explore/hooks/use-fetch-explore-asset.tsx
@@ -1,10 +1,36 @@
 import { useQuery } from "@tanstack/react-query";
 import { useTokenStore } from "../../auth/state/store";
+import { TFetchExploreAsset } from "../types/explore.types";
+import { err, ok, ResultAsync } from "neverthrow";
+import { fetchAllExploreAssets } from "../api/fetch-asset/route";
+import { ExploreError } from "../errors/explore.error";
+import { FetchExploreAssetDto } from "../dto/explore.dto";
+import { ExploreErrorMessages } from "../data/explore.data";
 
-export const useFetchExploreAsset = () => {
+export const useFetchExploreAsset = (params: TFetchExploreAsset) => {
   const id = useTokenStore((state) => state.user?.id);
   return useQuery({
-    queryKey: [`fetch-explore-${id}-asset`],
-    queryFn: async () => {},
+    queryKey: [`fetch-explore-${id}-asset`, params],
+    queryFn: async () => {
+      const result = await ResultAsync.fromPromise(
+        fetchAllExploreAssets(params),
+        (error) => new ExploreError(`Explore error ${error}`),
+      ).andThen((data) => {
+        const parsedResult = FetchExploreAssetDto.safeParse(data);
+
+        if (parsedResult.success) {
+          return ok(parsedResult.data);
+        } else {
+          return err(
+            new ExploreError(ExploreErrorMessages.FAILED_TO_FETCH_ASSET),
+          );
+        }
+      });
+      if (result.isErr()) {
+        throw result.error;
+      }
+
+      return result.value;
+    },
   });
 };
