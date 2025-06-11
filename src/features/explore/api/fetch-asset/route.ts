1c8a07a8443efb7784f328bfeb72029d0c7030e2
3illBaby <thrillanon@outlook.com>
2025-06-11 21:53:09 +0100

    chore(route.ts): added api to handle fetching asset

---
+++ src/features/explore/api/fetch-asset/route.ts
@@ -0,0 +1,26 @@
+import { ApiClient } from "@/src/lib/api-client";
+import { EXPLORE_OP } from "../../data/explore.route";
+import { TFetchExploreAsset } from "../../types/explore.types";
+
+export async function fetchAllExploreAssets(ctx: TFetchExploreAsset) {
+  let path = `${EXPLORE_OP.ALL_ASSET}`;
+  const queryParams = new URLSearchParams();
+
+  Object.keys(ctx).forEach((key) => {
+    const value = ctx[key as keyof TFetchExploreAsset];
+
+    if (value !== undefined && value !== null) {
+      if (key === "categories" && Array.isArray(value)) {
+        queryParams.append(key, value.join(","));
+      } else {
+        queryParams.append(key, String(value));
+      }
+    }
+  });
+
+  if (queryParams.toString()) {
+    path += `?${queryParams.toString()}`;
+  }
+
+  return await ApiClient.get(path);
+}
