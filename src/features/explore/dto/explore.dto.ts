c04d183a9d316f2a52ada208310885323668676a
3illBaby <thrillanon@outlook.com>
2025-06-11 22:40:09 +0100

    chore(explore.dto.ts): added response dto

---
+++ src/features/explore/dto/explore.dto.ts
@@ -0,0 +1,43 @@
+import { z } from "zod";
+
+export const Asset = z.object({
+  fileId: z.string(),
+  fileName: z.string(),
+  userId: z.string().uuid(),
+  imageUrl: z.string().url(),
+  firstName: z.string().min(2).max(100),
+  lastName: z.string().min(2).max(100),
+  price: z.number().min(0),
+  availability: z.boolean(),
+  categories: z.array(z.string()).min(1),
+  createdAt: z.date(),
+  isListed: z.boolean(),
+  isLocked: z.boolean(),
+  url: z.string().url(),
+  numOfLikes: z.number().min(0),
+});
+
+export const Pagination = z.object({
+  currentPage: z.number().min(1),
+  totalPages: z.number().min(1).max(100),
+  totalItems: z.number(),
+  itemsPerPage: z.number(),
+  hasNextPage: z.boolean(),
+  hasPreviousPage: z.boolean(),
+});
+
+export const Filters = z.object({
+  categories: z.string().optional(),
+  priceRange: z.nullable(z.tuple([z.number().min(0), z.number().min(0)])),
+  sortBy: z.string(),
+  available: z.boolean().optional(),
+});
+
+export const FetchExploreAssetDto = z.object({
+  status: z.number().min(0).max(1),
+  data: z.object({
+    assets: z.array(Asset),
+  }),
+  pagination: Pagination,
+  filters: Filters,
+});
