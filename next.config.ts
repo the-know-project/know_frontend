c3c02074ec31913a48383852e2cce8e1be3225d9
3illBaby <thrillanon@outlook.com>
2025-06-12 00:19:09 +0100

    Allow image uploads from imagekit.io domain

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,10 @@ const nextConfig: NextConfig = {
         hostname: "lh3.googleusercontent.com",
         port: "",
       },
+      {
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+      },
     ],
   },
 };
