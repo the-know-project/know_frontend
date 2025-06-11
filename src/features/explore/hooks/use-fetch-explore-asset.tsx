99d0faa636d6905633a311735bf5d715a7b05173
3illBaby <thrillanon@outlook.com>
2025-06-12 00:20:38 +0100

    Remove unused DTO validation and simplify explore asset fetch

--- src/features/explore/hooks/use-fetch-explore-asset.tsx
+++ src/features/explore/hooks/use-fetch-explore-asset.tsx
@@ -2,22 +2,19 @@ import { useQuery } from "@tanstack/react-query";
 import { err, ok, ResultAsync } from "neverthrow";
 import { fetchAllExploreAssets } from "../api/fetch-asset/route";
 import { ExploreErrorMessages } from "../data/explore.data";
-import { FetchExploreAssetDto } from "../dto/explore.dto";
 import { ExploreError } from "../errors/explore.error";
 import { TFetchExploreAsset } from "../types/explore.types";
 
 export const useFetchExploreAsset = (params: TFetchExploreAsset) => {
   return useQuery({
-    queryKey: [`fetch-explore-asset`, params],
+    queryKey: [`fetch-explore-asset`],
     queryFn: async () => {
       const result = await ResultAsync.fromPromise(
         fetchAllExploreAssets(params),
         (error) => new ExploreError(`Explore error ${error}`),
       ).andThen((data) => {
-        const parsedResult = FetchExploreAssetDto.safeParse(data);
-
-        if (parsedResult.success) {
-          return ok(parsedResult.data);
+        if (data.status === 200) {
+          return ok(data);
         } else {
           return err(
             new ExploreError(ExploreErrorMessages.FAILED_TO_FETCH_ASSET),
@@ -28,6 +25,7 @@ export const useFetchExploreAsset = (params: TFetchExploreAsset) => {
         throw result.error;
       }
 
+      console.log(result);
       return result.value;
     },
   });
