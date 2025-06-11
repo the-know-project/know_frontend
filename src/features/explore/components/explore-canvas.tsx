6ed3f60fa6df97ca81b4619fda9cb0ef1e6214a1
3illBaby <thrillanon@outlook.com>
2025-06-11 23:04:16 +0100

    chore(explore-canvas.tsx): consumed hook and updated data being sent to explore card
    handled loading state

--- src/features/explore/components/explore-canvas.tsx
+++ src/features/explore/components/explore-canvas.tsx
@@ -1,25 +1,35 @@
-import { ExploreDemoItem } from "@/src/constants/constants";
+"use client";
+
 import ExploreCard from "./explore-card";
+import { useFetchExploreAsset } from "../hooks/use-fetch-explore-asset";
+import { ExploreCardSkeletonGrid } from "./explore-card-skeleton";
 
 const ExploreCanvas = () => {
+  const { data, isLoading } = useFetchExploreAsset({});
+
+  if (isLoading) {
+    return <ExploreCardSkeletonGrid />;
+  }
+
+  const assets = data?.data.assets ? data.data.assets : [];
   return (
     <section className="flex w-full flex-col items-center justify-center">
       <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
-        {ExploreDemoItem.map((item, index) => (
+        {assets.map((item, index) => (
           <div
-            key={item.id}
+            key={item.fileId}
             className="motion-preset-expand motion-duration-700"
             style={{
               animationDelay: `${index * 100}ms`,
             }}
           >
             <ExploreCard
-              id={item.id}
-              artWork={item.artWork}
-              artName={item.artName}
-              artistImage={item.artistImage}
-              artistName={item.artistName}
-              likeCount={item.likeCount}
+              id={item.fileId}
+              artWork={item.url}
+              artName={item.fileName}
+              artistImage={item.imageUrl}
+              artistName={`${item.firstName} ${item.lastName}`}
+              likeCount={item.numOfLikes}
             />
           </div>
         ))}
