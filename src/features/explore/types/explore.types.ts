4aa33f263ecfd56314217ca644ee028ff1e51ed9
3illBaby <thrillanon@outlook.com>
2025-06-12 00:21:02 +0100

    Add types for Asset inference from zod schema

--- src/features/explore/types/explore.types.ts
+++ src/features/explore/types/explore.types.ts
@@ -1,4 +1,6 @@
 import { z } from "zod";
 import { FetchAssetSchema } from "../schema/explore.schema";
+import { Asset } from "../dto/explore.dto";
 
 export type TFetchExploreAsset = z.infer<typeof FetchAssetSchema>;
+export type TAsset = z.infer<typeof Asset>;
