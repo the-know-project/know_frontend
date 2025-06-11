a0934b465151bd7b26c15c807978bd2b632c1b4a
3illBaby <thrillanon@outlook.com>
2025-06-11 11:02:37 +0100

    refactor(route.ts): convert to form data before sending to the server

--- src/features/upload/api/upload/route.ts
+++ src/features/upload/api/upload/route.ts
@@ -1,7 +1,14 @@
 import { ApiClient } from "@/src/lib/api-client";
 import { IUploadAssetServer } from "../../types/upload.types";
 import { ASSET_OP } from "../../data/upload.route";
+import { convertToFormDataWithJSON } from "../../utils/form-data.utils";
 
 export async function uploadAsset(ctx: IUploadAssetServer) {
-  return ApiClient.post(ASSET_OP.UPLOAD, ctx);
+  const formData = convertToFormDataWithJSON(ctx);
+
+  return ApiClient.post(ASSET_OP.UPLOAD, formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
 }
