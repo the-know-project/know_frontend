84eae7b15a90c86b6640f415f3beb31d65654e89
3illBaby <thrillanon@outlook.com>
2025-06-17 14:11:05 +0100

    feat(use-like-asset.tsx): impl hook to like asset

---
+++ src/features/explore/hooks/use-like-asset.tsx
@@ -0,0 +1,38 @@
+import { useMutation } from "@tanstack/react-query";
+import { useTokenStore } from "../../auth/state/store";
+import { err, ok, ResultAsync } from "neverthrow";
+import { likeAsset } from "../api/like-asset/route";
+import { TLikeAsset } from "../types/explore.types";
+import { ExploreError } from "../errors/explore.error";
+
+export const useLikeAsset = () => {
+  const userId = useTokenStore((state) => state.user?.id);
+  if (!userId) {
+    throw new ExploreError("User not authenticated");
+  }
+  return useMutation({
+    mutationKey: [`like-asset-${userId}`],
+    mutationFn: async (ctx: Pick<TLikeAsset, "fileId">) => {
+      const result = await ResultAsync.fromPromise(
+        likeAsset({
+          userId,
+          fileId: ctx.fileId,
+        }),
+        (error) => new ExploreError(`Error liking asset ${error}`),
+      ).andThen((data) => {
+        if (data.status === 200) {
+          return ok(data);
+        } else {
+          return err(new ExploreError(`Error liking asset ${data.status}`));
+        }
+      });
+
+      if (result.isErr()) {
+        return result.error;
+      }
+
+      console.log(result);
+      return result.value;
+    },
+  });
+};
