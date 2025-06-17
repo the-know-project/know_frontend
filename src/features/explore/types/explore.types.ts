3c7b6b88e2f02ff437b8faf0a32ad81e6c7084da
3illBaby <thrillanon@outlook.com>
2025-06-17 13:52:15 +0100

    chore(explore.types.ts): defined like asset type

--- src/features/explore/types/explore.types.ts
+++ src/features/explore/types/explore.types.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
-import { FetchAssetSchema } from "../schema/explore.schema";
+import { FetchAssetSchema, LikeAssetSchema } from "../schema/explore.schema";
 import { Asset } from "../dto/explore.dto";
 
 export type TFetchExploreAsset = z.infer<typeof FetchAssetSchema>;
 export type TAsset = z.infer<typeof Asset>;
+export type TLikeAsset = z.infer<typeof LikeAssetSchema>;
