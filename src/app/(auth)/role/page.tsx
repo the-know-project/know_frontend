afc2b8faca3859558a96b3d13f38858052897670
3illBaby <thrillanon@outlook.com>
2025-06-18 13:53:31 +0100

    Add auth guard to role selection page

--- src/app/(auth)/role/page.tsx
+++ src/app/(auth)/role/page.tsx
@@ -1,15 +1,18 @@
+import { PageAuthGuard } from "@/src/features/auth/guards";
 import GridBackground from "@/src/shared/components/grid-background";
 import SelectRole from "@/src/shared/components/role-selection";
 
 const Page = () => {
   return (
-    <section className="flex w-full flex-col scroll-smooth">
-      <GridBackground>
-        <>
-          <SelectRole />
-        </>
-      </GridBackground>
-    </section>
+    <PageAuthGuard guestOnly>
+      <section className="flex w-full flex-col scroll-smooth">
+        <GridBackground>
+          <>
+            <SelectRole />
+          </>
+        </GridBackground>
+      </section>
+    </PageAuthGuard>
   );
 };
 
