9dcd23fac8f153500cad0c1fc334371f62dbb9db
3illBaby <thrillanon@outlook.com>
2025-06-20 16:06:19 +0100

    Add glassmorphism effect to notification card

--- src/shared/components/notification-card.tsx
+++ src/shared/components/notification-card.tsx
@@ -5,8 +5,12 @@ import Image from "next/image";
 const NotificationCard = () => {
   const data = MockNotifications;
   return (
-    <section className="notification_card">
-      <div className="flex w-full flex-col gap-5">
+    <section className="relative flex min-h-[300px] min-w-[300px] flex-col overflow-hidden rounded-[15px] border border-white/20 bg-white/5 px-2 py-4 shadow-[0_8px_32px_0_rgba(31,38,135,0.37),inset_0_1px_0_0_rgba(255,255,255,0.18)]">
+      {/* Pseudo-element for additional glass effect */}
+      {/* I used this because the backdrop utility is not very compatible with mobile browsers */}
+      <div className="pointer-events-none absolute inset-0 rounded-[15px] bg-gradient-to-br from-white/10 to-transparent"></div>
+
+      <div className="relative z-10 flex w-full flex-col gap-5">
         {data.map((notification, index) => (
           <div
             key={notification.id}
