52158fa251a9c5613752bc7e38b043324805109d
3illBaby <thrillanon@outlook.com>
2025-06-22 22:32:49 +0100

    chore(notifications.route.ts): impl api route

--- src/features/notifications/data/notifications.route.ts
+++ src/features/notifications/data/notifications.route.ts
@@ -1,3 +1,4 @@
 export enum NOTIFICATIONS_OP {
   FETCH_USER_NOTIFICATIONS = "/api/notifications/fetchUserNotifications",
+  DELETE_USER_NOTIFICATIONS = "/api/notifications/deleteUserNotifications",
 }
