f9aee118d1aaa730dbc7b66b8de1c8b1a4cf56cf
3illBaby <thrillanon@outlook.com>
2025-06-22 23:17:31 +0100

    Add delete user notifications mutation hook

---
+++ src/features/notifications/hooks/use-delete-user-notifications.tsx
@@ -0,0 +1,46 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { err, ok, ResultAsync } from "neverthrow";
+import { useTokenStore } from "../../auth/state/store";
+import { deleteNotifications } from "../api/delete-notifications/route";
+import { NotificationError } from "../error/notification.error";
+import { IDeleteNotifications } from "../types/notification.types";
+import { DeleteNotificationsResponseDto } from "../dto/notifications.dto";
+
+export const useDeleteUserNotifications = () => {
+  const queryClient = useQueryClient();
+  const user = useTokenStore((state) => state.user);
+
+  return useMutation({
+    mutationKey: [`delete-user-notifications-${user?.id}`],
+    mutationFn: async (ctx: Omit<IDeleteNotifications, "userId">) => {
+      const result = await ResultAsync.fromPromise(
+        deleteNotifications(ctx),
+        (error: any) =>
+          new NotificationError(`Error deleting notifications: ${error}`),
+      ).andThen((data) => {
+        if (data.status === 200) {
+          return ok(data);
+        } else {
+          return err(
+            new NotificationError(
+              `Error deleteing notifciations ${data.message}`,
+            ),
+          );
+        }
+      });
+
+      if (result.isErr()) {
+        return result.error;
+      }
+
+      console.log(result.value);
+      const validatedData = DeleteNotificationsResponseDto.parse(result.value);
+      return validatedData;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [`fetch-user-notifications-${user?.id}`],
+      });
+    },
+  });
+};
