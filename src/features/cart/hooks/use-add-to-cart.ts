3307e059f45c7887726dad4d309acf0c503f846f
3illBaby <thrillanon@outlook.com>
2025-06-25 22:54:03 +0100

    Add useAddToCart hook for managing cart mutations

---
+++ src/features/cart/hooks/use-add-to-cart.ts
@@ -0,0 +1,54 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useTokenStore } from "../../auth/state/store";
+import { err, ok, ResultAsync } from "neverthrow";
+import { addToCart } from "../api/add-to-cart/route";
+import { CartError } from "../error/cart.error";
+
+export const useAddToCart = () => {
+  const queryClient = useQueryClient();
+  const userId = useTokenStore((state) => state.user?.id);
+
+  if (!userId) {
+    return {
+      mutateAsync: async () => {},
+      isPending: false,
+      isSuccess: false,
+      isError: false,
+      error: null,
+      data: null,
+    };
+  }
+  return useMutation({
+    mutationKey: [`add-to-cart-${userId}`],
+    mutationFn: async (fileId: string) => {
+      const result = await ResultAsync.fromPromise(
+        addToCart({
+          userId,
+          fileId,
+        }),
+        (error) => new CartError(`Error adding item to cart ${error}`),
+      ).andThen((data) => {
+        if (data.status === 200) {
+          return ok(data);
+        } else {
+          return err(
+            new CartError(`An error occurred while calling function ${data}`),
+          );
+        }
+      });
+
+      if (result.isErr()) {
+        return result.error;
+      }
+
+      console.log(result.value);
+      return result.value;
+    },
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [`fetch-user-cart-${userId}`],
+      });
+    },
+  });
+};
