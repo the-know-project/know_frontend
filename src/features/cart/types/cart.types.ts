c82d406a63c50ee1f6987cc6ab14a535e534d157
3illBaby <thrillanon@outlook.com>
2025-06-25 22:53:40 +0100

    Add AddToCart type inference from zod schema

--- src/features/cart/types/cart.types.ts
+++ src/features/cart/types/cart.types.ts
@@ -1,4 +1,6 @@
 import { z } from "zod";
 import { CartData } from "../dto/cart.dto";
+import { AddToCartSchema } from "../schema/cart.schema";
 
 export type TCart = z.infer<typeof CartData>;
+export type IAddToCart = z.infer<typeof AddToCartSchema>;
