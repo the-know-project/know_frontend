0f882a29a99a7a87b014805f53239ebe862bbb82
3illBaby <thrillanon@outlook.com>
2025-06-25 16:13:43 +0100

    Add useFetchUserCart hook with error handling

---
+++ src/features/cart/hooks/use-fetch-user-cart.ts
@@ -0,0 +1,37 @@
+import { useQuery } from "@tanstack/react-query";
+import { useTokenStore } from "../../auth/state/store";
+import { err, ok, ResultAsync } from "neverthrow";
+import { fetchUserCart } from "../api/fetch-user-cart/route";
+import { CartError } from "../error/cart.error";
+
+export const useFetchUserCart = () => {
+  const userId = useTokenStore((state) => state.user?.id);
+  if (!userId) {
+    return [];
+  }
+  return useQuery({
+    queryKey: [`fetch-user-cart-${userId}`],
+    queryFn: async () => {
+      const result = await ResultAsync.fromPromise(
+        fetchUserCart(userId),
+        (error) => new CartError(`Error fetching user cart ${error}`),
+      ).andThen((data) => {
+        if (data.status === 200) {
+          return ok(data);
+        } else {
+          return err(
+            new CartError(`Error fetching user cart: ${data.message}`),
+          );
+        }
+      });
+
+      if (result.isErr()) {
+        throw result.error;
+      }
+
+      console.log(result.value);
+      return result.value;
+    },
+    staleTime: 5000,
+  });
+};
