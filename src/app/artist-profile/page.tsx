8f6545be7886fe6bb1891c9841031e834d941e51
3illBaby <thrillanon@outlook.com>
2025-06-28 00:37:08 +0100

    chore(page.tsx): page.tsx
    protected pages

--- src/app/artist-profile/page.tsx
+++ src/app/artist-profile/page.tsx
@@ -1,15 +1,18 @@
+import { PageAuthGuard } from "@/src/features/auth/guards";
 import ExploreNav from "@/src/features/explore/components/explore-nav";
 import PostGrid from "@/src/shared/components/postgrid";
 
 export default function Page() {
   return (
-    <div className="flex min-h-screen">
-      <div className="flex flex-1 flex-col">
-        <ExploreNav />
-        <main className="p-6">
-          <PostGrid />
-        </main>
+    <PageAuthGuard requiredRoles={["ARTIST"]} requiresAuth={true}>
+      <div className="flex min-h-screen">
+        <div className="flex flex-1 flex-col">
+          <ExploreNav />
+          <main className="p-6">
+            <PostGrid />
+          </main>
+        </div>
       </div>
-    </div>
+    </PageAuthGuard>
   );
 }
