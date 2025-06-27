de355f154737bac06ec55523a6d796b4edbaf82a
3illBaby <thrillanon@outlook.com>
2025-06-27 17:37:51 +0100

    chore(profile-modal.tsx): init profile modal

---
+++ src/features/profile/components/profile-modal.tsx
@@ -0,0 +1,9 @@
+const ProfileModal = () => {
+  return (
+    <section className="scrollbar-hide relative flex max-h-[600px] min-h-[300px] min-w-[300px] scroll-m-2 flex-col overflow-auto scroll-smooth rounded-[15px] border border-white/20 bg-white px-2 py-4 opacity-95 shadow-[0_8px_32px_0_rgba(31,38,135,0.37),inset_0_1px_0_0_rgba(255,255,255,0.18)]">
+      <p>Profile Modal</p>
+    </section>
+  );
+};
+
+export default ProfileModal;
