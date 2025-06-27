d73a3e4e8d060b1aaf2d859e4910684d9b13f0d1
3illBaby <thrillanon@outlook.com>
2025-06-28 00:37:40 +0100

    chore(page.tsx): protected route

--- src/app/buyer-profile/page.tsx
+++ src/app/buyer-profile/page.tsx
@@ -1,10 +1,14 @@
-const BuyerProfilePage = () => {
+import { PageAuthGuard } from "@/src/features/auth/guards";
+
+const Page = () => {
   return (
-    <div>
-      <h1>Buyer Profile</h1>
-      <p>This is the buyer profile page.</p>
-    </div>
+    <PageAuthGuard requiredRoles={["BUYER"]} requiresAuth={true}>
+      <div>
+        <h1>Buyer Profile</h1>
+        <p>This is the buyer profile page.</p>
+      </div>
+    </PageAuthGuard>
   );
 };
 
-export default BuyerProfilePage;
+export default Page;
