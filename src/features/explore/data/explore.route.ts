e43d1fa41c9904cbd3fc0ee9262f3fdcbb5d0633
3illBaby <thrillanon@outlook.com>
2025-06-27 10:09:55 +0100

    chore(explore.route.ts): extended routes

--- src/features/explore/data/explore.route.ts
+++ src/features/explore/data/explore.route.ts
@@ -2,4 +2,5 @@ export enum EXPLORE_OP {
   ALL_ASSET = "/api/asset/allAssets",
   LIKE_ASSET = "/api/asset/likeAsset",
   UNLIKE_ASSET = "/api/asset/unLikeAsset",
+  FETCH_USER_LIKES = "/api/user/fetchUserLikes",
 }
