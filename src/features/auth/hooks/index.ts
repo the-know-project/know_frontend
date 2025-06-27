5ad37ada32514d33322bf146bdf84f8dd005f31d
3illBaby <thrillanon@outlook.com>
2025-06-27 22:51:42 +0100

    Remove deprecated auth status hooks

--- src/features/auth/hooks/index.ts
+++ src/features/auth/hooks/index.ts
@@ -2,6 +2,4 @@ export { useAuth } from "./use-auth";
 export { useLogin } from "./use-login";
 export { useLoginWithRole } from "./use-login-with-role";
 export { useLogout } from "./use-logout";
-export { useAuthStatus } from "./use-auth-status";
-export { useSafeAuthStatus } from "./use-safe-auth-status";
 export { useTokenRefresh } from "./use-token-refresh";
