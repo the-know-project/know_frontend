6aa99e098acce10eb376f09d71c78cd261c6428d
3illBaby <thrillanon@outlook.com>
2025-07-02 15:20:05 +0100

    Add description form and reorder editor sections

--- src/features/upload/components/upload-editor.tsx
+++ src/features/upload/components/upload-editor.tsx
@@ -1,19 +1,24 @@
 import SizePickerForm from "./size-picker.form";
 import TagPickerForm from "./tag-picker.form";
 import CategoryPickerForm from "./category-picker.form";
+import DescriptionForm from "./description.form";
 
 const UploadEditor = () => {
   return (
-    <section className="flex w-full flex-col gap-[100px]">
-      {/* Size Editor */}
+    <section className="flex w-full flex-col gap-[50px]">
       <div className="flex flex-col items-start gap-3">
-        <h3 className="editor_title_font">edit size</h3>
-        <SizePickerForm />
+        <h3 className="editor_title_font">Description</h3>
+        <DescriptionForm />
       </div>
 
       <div className="flex flex-col items-start gap-3">
         <h3 className="editor_title_font">Add Tags</h3>
         <TagPickerForm />
+      </div>
+
+      <div className="flex flex-col items-start gap-3">
+        <h3 className="editor_title_font">edit size</h3>
+        <SizePickerForm />
       </div>
 
       <div className="flex flex-col items-start gap-3">
