0aeceeb8a1d0254272b5411f9b2017fbbdd085de
3illBaby <thrillanon@outlook.com>
2025-07-06 23:52:13 +0100

    Add artist profile navigation and adjust layout spacing

--- src/app/artist-profile/layout.tsx
+++ src/app/artist-profile/layout.tsx
@@ -1,3 +1,4 @@
+import ExploreNav from "@/src/features/explore/components/explore-nav";
 import { Sidebar } from "@/src/features/profile";
 
 interface ILayout {
@@ -6,9 +7,15 @@ interface ILayout {
 
 const Layout: React.FC<ILayout> = ({ children }) => {
   return (
-    <div className="flex min-h-screen">
-      <Sidebar />
-      <main className="flex-1">{children}</main>
+    <div className="flex min-h-screen flex-col">
+      <ExploreNav />
+
+      <div className="mt-[80px] flex w-full gap-[50px]">
+        <div className="flex-start slide-in-from-bottom flex flex-col">
+          <Sidebar />
+        </div>
+        <main className="flex-1">{children}</main>
+      </div>
     </div>
   );
 };
