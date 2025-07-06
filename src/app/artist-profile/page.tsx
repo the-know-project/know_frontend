699b0341966218ed0bedfda1fac310055e5a1eb8
3illBaby <thrillanon@outlook.com>
2025-07-06 23:52:29 +0100

    Remove explore nav from artist profile page

--- src/app/artist-profile/page.tsx
+++ src/app/artist-profile/page.tsx
@@ -1,5 +1,4 @@
 import { PageAuthGuard } from "@/src/features/auth/guards";
-import ExploreNav from "@/src/features/explore/components/explore-nav";
 import PostGrid from "@/src/shared/components/postgrid";
 
 export default function Page() {
@@ -7,7 +6,6 @@ export default function Page() {
     <PageAuthGuard requiredRoles={["ARTIST"]} requiresAuth={true}>
       <div className="flex min-h-screen">
         <div className="flex flex-1 flex-col">
-          <ExploreNav />
           <main className="p-6">
             <PostGrid />
           </main>
