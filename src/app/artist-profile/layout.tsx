3cd660b9f83af9dfb6ad223e759834d694b0ff60
3illBaby <thrillanon@outlook.com>
2025-07-10 16:35:59 +0100

    Add wrapper div for nav component styling

--- src/app/artist-profile/layout.tsx
+++ src/app/artist-profile/layout.tsx
@@ -8,7 +8,9 @@ interface ILayout {
 const Layout: React.FC<ILayout> = ({ children }) => {
   return (
     <div className="flex min-h-screen flex-col">
-      <ExploreNav />
+      <div className="mt-2 flex w-full flex-col">
+        <ExploreNav />
+      </div>
 
       <div className="mt-[80px] flex w-full gap-[50px]">
         <div className="flex-start slide-in-from-bottom flex flex-col">
