475881a6489e30444ce4539c2d148b63d3c8e005
3illBaby <thrillanon@outlook.com>
2025-07-10 20:47:19 +0100

    refactor(profile-grid.tsx): refactored component to conditionally render grid view

--- src/features/profile/components/profile-grid.tsx
+++ src/features/profile/components/profile-grid.tsx
@@ -1,55 +1,22 @@
 "use client";
-import { ArtistProfileToggle, posts } from "@/src/constants/constants";
-import { useState } from "react";
-import ProfileCard from "./profile-card";
+import { useStableAuthStatus } from "../../auth/hooks/use-stable-auth-status";
+import ArtistProfileGrid from "../artist/components/artist-profile-grid";
 
 export const ProfileGrid = () => {
-  const [activeToggle, setActiveToggle] = useState<string>("posts");
+  const { role } = useStableAuthStatus({
+    redirectOnExpiry: true,
+    redirectTo: "/login",
+  });
+
   return (
-    <section className="flex w-full flex-col">
-      <div className="relative flex flex-row items-center justify-between">
-        {ArtistProfileToggle.map((toggle) => (
-          <button
-            key={toggle.id}
-            className="flex flex-1 flex-col items-start"
-            onClick={() => setActiveToggle(toggle.name)}
-          >
-            <p
-              className={`font-bricolage capitalize hover:scale-105 active:scale-95 ${
-                activeToggle === toggle.name
-                  ? "font-semibold text-black transition-colors duration-300"
-                  : "text-neutral-500"
-              }`}
-            >
-              {toggle.name}
-            </p>
-          </button>
-        ))}
-        {/* Continuous line background */}
-        <div className="absolute right-0 bottom-0 left-0 h-[2px] bg-gray-300"></div>
-        {/* Active section highlight */}
-        <div
-          className={`absolute bottom-0 h-[2px] bg-black transition-all duration-300 ${
-            activeToggle === "posts"
-              ? "left-0 w-1/4"
-              : activeToggle === "stats"
-                ? "left-1/4 w-1/4"
-                : "left-[67%] w-[33%]"
-          }`}
-        ></div>
-      </div>
-      <div className="mt-1 grid grid-cols-2 gap-4 bg-white">
-        {posts.map((post) => (
-          <ProfileCard
-            key={post.id}
-            id={post.id}
-            title={post.title}
-            views={post.views}
-            createdAt={post.createdAt}
-            image={post.image}
-          />
-        ))}
-      </div>
-    </section>
+    <>
+      {role === "ARTIST" ? (
+        <ArtistProfileGrid />
+      ) : (
+        <div>
+          <p>Buyer profile grid should be implemented here</p>
+        </div>
+      )}
+    </>
   );
 };
