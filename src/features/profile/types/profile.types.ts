0f453e870539d492547356a09c4c941c67b6fae3
3illBaby <thrillanon@outlook.com>
2025-07-11 10:33:50 +0100

    Add type export for IFetchUserAsset

--- src/features/profile/types/profile.types.ts
+++ src/features/profile/types/profile.types.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
-import { ArtistMetricsDto } from "../artist/dto/artist.dto";
+import { ArtistMetricsDto, FetchUserAsset } from "../artist/dto/artist.dto";
 
 export type IArtistMetricsDto = z.infer<typeof ArtistMetricsDto>;
+export type IFetchUserAsset = z.infer<typeof FetchUserAsset>;
