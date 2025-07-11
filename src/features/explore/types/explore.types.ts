e6bcf9f0867968b01f12277c02e699dd7af80b70
3illBaby <thrillanon@outlook.com>
2025-07-11 08:31:14 +0100

    Add TExploreAssetDto type for fetch explore asset DTO

--- src/features/explore/types/explore.types.ts
+++ src/features/explore/types/explore.types.ts
@@ -1,7 +1,8 @@
 import { z } from "zod";
 import { FetchAssetSchema, LikeAssetSchema } from "../schema/explore.schema";
-import { Asset } from "../dto/explore.dto";
+import { Asset, FetchExploreAssetDto } from "../dto/explore.dto";
 
 export type TFetchExploreAsset = z.infer<typeof FetchAssetSchema>;
 export type TAsset = z.infer<typeof Asset>;
 export type TLikeAsset = z.infer<typeof LikeAssetSchema>;
+export type TExploreAssetDto = z.infer<typeof FetchExploreAssetDto>;
