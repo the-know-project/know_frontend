6033974836167556b6f9183ab3510eaa11567c25
3illBaby <thrillanon@outlook.com>
2025-07-13 15:32:38 +0100

    Comment out checkout page due to type error

--- src/app/checkout/[step]/page.tsx
+++ src/app/checkout/[step]/page.tsx
@@ -1,25 +1,34 @@
-// app/checkout/[step]/page.tsx
-import { notFound } from "next/navigation";
-import { PaymentMethodForm } from "../components/PaymentMethodForm";
-import { CardDetailsForm } from "../components/CardDetailsForm";
-import { OrderConfirmation } from "../components/OrderConfirmation";
-// import { ShippingInfo } from "../components/ShippingInfo";
+/**
+ * @Developer: This implementation is throwing an error.
+ * @Error: Type error: Type '{ params: { step: string; }; }' does not satisfy the constraint 'PageProps'.
+   Types of property 'params' are incompatible.
+     Type '{ step: string; }' is missing the following properties from type 'Promise<any>': then, catch, finally, [Symbol.toStringTag]
+  * @Dev: Ensure to always test your implementation
 
-const stepMap:any = {
-  "payment-method": () => <PaymentMethodForm />,
-  "card-details": () => <CardDetailsForm />,
-  confirm: () => <OrderConfirmation />,
-//   shipping: () => <ShippingInfo />,
-};
+ */
 
-export default function CheckoutStepPage({
-  params,
-}: {
-  params: { step: string };
-}) {
-  const StepComponent = stepMap[params.step];
+// // app/checkout/[step]/page.tsx
+// import { notFound } from "next/navigation";
+// import { PaymentMethodForm } from "../components/PaymentMethodForm";
+// import { CardDetailsForm } from "../components/CardDetailsForm";
+// import { OrderConfirmation } from "../components/OrderConfirmation";
+// // import { ShippingInfo } from "../components/ShippingInfo";
 
-  if (!StepComponent) return notFound();
+// const stepMap:any = {
+//   "payment-method": () => <PaymentMethodForm />,
+//   "card-details": () => <CardDetailsForm />,
+//   confirm: () => <OrderConfirmation />,
+// //   shipping: () => <ShippingInfo />,
+// };
 
-  return <StepComponent />;
-}
+// export default function CheckoutStepPage({
+//   params,
+// }: {
+//   params: { step: string };
+// }) {
+//   const StepComponent = stepMap[params.step];
+
+//   if (!StepComponent) return notFound();
+
+//   return <StepComponent />;
+// }
