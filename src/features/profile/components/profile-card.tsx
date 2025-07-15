a1941bda636c74a7cb915aeb239cab86bae30e96
3illBaby <thrillanon@outlook.com>
2025-07-15 14:25:00 +0100

    Make profile card component responsive

--- src/features/profile/components/profile-card.tsx
+++ src/features/profile/components/profile-card.tsx
@@ -18,32 +18,40 @@ const ProfileCard: React.FC<IProfileCard> = ({
   image,
 }) => {
   return (
-    <div className="flex h-[400px] w-[500px] flex-col">
-      <Image
-        src={image}
-        alt="user_asset"
-        quality={100}
-        width={500}
-        height={300}
-        className="rounded-[15px] object-contain select-none"
-      />
+    <div className="mt-[30px] flex w-full flex-col px-4">
+      <div className="flex flex-col">
+        <Image
+          src={image}
+          alt="user_asset"
+          quality={100}
+          width={400}
+          height={300}
+          className="rounded-[15px] object-contain select-none"
+        />
 
-      <div className="mt-2 flex justify-between">
-        <div className="flex flex-col items-start font-medium text-neutral-600">
-          <h3 className="font-bricolage capitalize">{title}</h3>
-          <p className="font-bricolage text-sm capitalize">{createdAt}</p>
-        </div>
-
-        <div className="flex items-center gap-3 text-black">
-          <div className="flex items-center gap-1">
-            <IconEye width={15} height={15} />
-            <p className="font-bricolage text-sm capitalize">
-              {formatViewCount(views)}
+        <div className="mt-2 flex w-full max-w-[400px] justify-between">
+          <div className="flex flex-col items-start font-medium text-neutral-600">
+            <h3 className="font-bricolage sm:text-normal text-sm capitalize">
+              {title}
+            </h3>
+            <p className="font-bricolage text-xs capitalize sm:text-sm">
+              {createdAt}
             </p>
           </div>
-          <div className="flex items-center gap-1">
-            <IconPencil width={15} height={15} />
-            <p className="font-bricolage text-sm capitalize">Edit</p>
+
+          <div className="flex items-center gap-3 text-black">
+            <div className="flex items-center gap-1">
+              <IconEye width={15} height={15} />
+              <p className="font-bricolage text-sm capitalize">
+                {formatViewCount(views)}
+              </p>
+            </div>
+            <div className="flex items-center gap-1">
+              <IconPencil width={15} height={15} />
+              <p className="font-bricolage text-xs capitalize sm:text-sm">
+                Edit
+              </p>
+            </div>
           </div>
         </div>
       </div>
