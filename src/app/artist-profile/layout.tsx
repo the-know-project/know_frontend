656563411cafeb17dcb2f4d09f9c266c36369a09
3illBaby <thrillanon@outlook.com>
2025-07-15 14:24:06 +0100

    Hide sidebar on mobile and disable share button

--- src/app/artist-profile/layout.tsx
+++ src/app/artist-profile/layout.tsx
@@ -9,11 +9,11 @@ const Layout: React.FC<ILayout> = ({ children }) => {
   return (
     <div className="flex min-h-screen flex-col">
       <div className="mt-2 flex w-full flex-col">
-        <ExploreNav />
+        <ExploreNav toggleShareButton={false} />
       </div>
 
       <div className="mt-[80px] flex w-full gap-[50px]">
-        <div className="flex-start slide-in-from-bottom flex flex-col">
+        <div className="lg:flex-start hidden lg:flex lg:flex-col">
           <Sidebar />
         </div>
         <main className="flex-1">{children}</main>
