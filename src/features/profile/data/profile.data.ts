d2c21e6a2fa130616bcb6821dbde1e6e34712785
3illBaby <thrillanon@outlook.com>
2025-07-26 12:34:38 +0100

    Add profile toggle options data structure

---
+++ src/features/profile/data/profile.data.ts
@@ -0,0 +1,18 @@
+export const ProfileToggleData = [
+  {
+    id: 1,
+    name: "Edit post",
+  },
+  {
+    id: 2,
+    name: "Share",
+  },
+  {
+    id: 3,
+    name: "Unpublish",
+  },
+  {
+    id: 4,
+    name: "Delete",
+  },
+];
