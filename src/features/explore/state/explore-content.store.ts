5af6bf38329560e9cbe88a9172b3a7c561babc76
3illBaby <thrillanon@outlook.com>
2025-07-31 14:24:26 +0100

    refactor(explore-content.store.ts): refactored to set and get asset details

--- src/features/explore/state/explore-content.store.ts
+++ src/features/explore/state/explore-content.store.ts
@@ -1,3 +1,4 @@
+import { IExploreContent } from "@/src/shared/hooks/interface/shared.interface";
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
@@ -5,10 +6,12 @@ import { immer } from "zustand/middleware/immer";
 interface IExploreContentState {
   toggleExploreContent: (
     id: string,
+    ctx: IExploreContent | null,
     viewportPosition?: { scrollY: number; viewportHeight: number },
   ) => void;
   toggledContentId: string | null;
   viewportPosition: { scrollY: number; viewportHeight: number } | null;
+  exploreContent: IExploreContent | null;
 }
 
 const useExploreContentStore = create<IExploreContentState>()(
@@ -16,15 +19,21 @@ const useExploreContentStore = create<IExploreContentState>()(
     immer((set) => ({
       toggleExploreContent: (
         id: string,
+        ctx: IExploreContent | null,
         viewportPosition?: { scrollY: number; viewportHeight: number },
       ) =>
         set((state) => ({
           toggledContentId: state.toggledContentId === id ? null : id,
           viewportPosition:
             state.toggledContentId === id ? null : viewportPosition || null,
+          exploreContent:
+            ctx && state.exploreContent && ctx.id === state.exploreContent.id
+              ? null
+              : ctx || null,
         })),
       toggledContentId: null,
       viewportPosition: null,
+      exploreContent: null,
     })),
     {
       name: "explore-content-store",
@@ -41,4 +50,5 @@ export const useIsExploreContentToggled = () => ({
   ),
   toggledContentId: useExploreContentStore((state) => state.toggledContentId),
   viewportPosition: useExploreContentStore((state) => state.viewportPosition),
+  exploreContent: useExploreContentStore((state) => state.exploreContent),
 });
