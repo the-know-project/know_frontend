dfb187a4bd2b3241e2edb632ecd85ed6637a2486
3illBaby <thrillanon@outlook.com>
2025-08-03 23:05:45 +0100

    Enhance art details modal with tags and categories

--- src/shared/components/art-details.tsx
+++ src/shared/components/art-details.tsx
@@ -7,7 +7,7 @@ import {
   useToggleExploreContent,
 } from "@/src/features/explore/state/explore-content.store";
 import { showLog } from "@/src/utils/logger";
-import { IconX } from "@tabler/icons-react";
+import { IconTag, IconX } from "@tabler/icons-react";
 import { AnimatePresence, motion } from "framer-motion";
 import Image from "next/image";
 import { useEffect } from "react";
@@ -24,17 +24,17 @@ const ArtDetails = () => {
   /**
    * @Note: This effect prevents body scrolling when the modal is open.
    */
-  useEffect(() => {
-    if (isExploreContentToggled) {
-      document.body.style.overflow = "hidden";
-    } else {
-      document.body.style.overflow = "unset";
-    }
+  // useEffect(() => {
+  //   if (isExploreContentToggled) {
+  //     document.body.style.overflow = "hidden";
+  //   } else {
+  //     document.body.style.overflow = "unset";
+  //   }
 
-    return () => {
-      document.body.style.overflow = "unset";
-    };
-  }, [isExploreContentToggled]);
+  //   return () => {
+  //     document.body.style.overflow = "unset";
+  //   };
+  // }, [isExploreContentToggled]);
 
   showLog({
     context: "Art-Details",
@@ -98,7 +98,7 @@ const ArtDetails = () => {
               ease: "easeInOut",
               delay: 0.05,
             }}
-            className="fixed z-50 mt-[50px] rounded-2xl bg-transparent shadow-2xl"
+            className="fixed z-50 rounded-2xl bg-transparent shadow-2xl"
             style={{
               top: viewportPosition
                 ? viewportPosition.scrollY +
@@ -117,13 +117,13 @@ const ArtDetails = () => {
               height:
                 typeof window !== "undefined" && window.innerWidth < 768
                   ? "83vh"
-                  : "83vh",
+                  : "95vh",
             }}
             onClick={(e) => e.stopPropagation()}
           >
-            <div className="flex h-full w-full flex-col">
+            <div className="flex h-full w-full flex-col overflow-hidden rounded-2xl">
               {/* Header */}
-              <div className="flex items-center justify-between px-6 py-4">
+              <div className="flex items-center justify-between bg-transparent px-4 py-4">
                 <div className="flex items-center gap-3">
                   <div className="flex h-8 w-8 items-center justify-center rounded-full bg-gradient-to-br from-blue-500 to-purple-600 p-1">
                     <Image
@@ -157,7 +157,7 @@ const ArtDetails = () => {
               </div>
 
               {/* Content */}
-              <div className="custom-scrollbar flex-1 overflow-auto rounded-2xl bg-[#141414]">
+              <div className="custom-scrollbar flex-1 overflow-auto bg-[#141414]">
                 <div className="space-y-6">
                   <div className="p-6">
                     <h2 className="font-bricolage mb-4 text-2xl font-bold text-white capitalize">
@@ -168,6 +168,21 @@ const ArtDetails = () => {
                         ? "Brought to you by Know, The ultimate partner for creatives."
                         : exploreContent?.description}
                     </p>
+
+                    {/*Tags*/}
+                    <div className="mt-2 flex items-center gap-2">
+                      {exploreContent?.tags?.map((tag, index) => (
+                        <span
+                          key={index}
+                          className="font-bebas motion-duration-500 motion-preset-expand flex items-center gap-1 rounded-full bg-neutral-600/50 px-2 py-1 text-xs font-medium tracking-wider text-white lowercase shadow-sm"
+                          style={{
+                            animationDelay: `${index * 150}ms`,
+                          }}
+                        >
+                          <IconTag width={15} height={15} /> {tag}
+                        </span>
+                      ))}
+                    </div>
                   </div>
 
                   {/* Artwork Image */}
@@ -186,6 +201,23 @@ const ArtDetails = () => {
                       </div>
                     </div>
                   </div>
+                </div>
+              </div>
+
+              {/* Categories */}
+              <div className="lg-mt-[50px] mt-0 border-t border-neutral-700 bg-[#141414] px-4 py-3">
+                <div className="flex flex-wrap">
+                  {exploreContent?.categories?.map((category, index) => (
+                    <span
+                      key={index}
+                      className="font-bebas motion-duration-500 motion-preset-expand mt-2 mr-2 rounded-full bg-neutral-600/50 px-2 py-1 text-xs font-medium tracking-wider text-white lowercase"
+                      style={{
+                        animationDelay: `${index * 150}ms`,
+                      }}
+                    >
+                      {category}
+                    </span>
+                  ))}
                 </div>
               </div>
             </div>
