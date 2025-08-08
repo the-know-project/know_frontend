37c00d1ed1803beed2fa59641004b072e0f5da7a
3illBaby <thrillanon@outlook.com>
2025-08-08 17:32:25 +0100

    Add dummy header metrics data for artist profile

---
+++ src/features/profile/artist/data/artist.data.ts
@@ -0,0 +1,22 @@
+export const DummyHeaderMetrics = [
+  {
+    id: 1,
+    context: "followers",
+    value: "100",
+  },
+  {
+    id: 2,
+    context: "post views",
+    value: "500",
+  },
+  {
+    id: 3,
+    context: "following",
+    value: "200",
+  },
+  {
+    id: 4,
+    context: "profile views",
+    value: "300",
+  },
+];
