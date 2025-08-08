cb3107bc678fdd4638f4c104eaea4117bf83adf3
3illBaby <thrillanon@outlook.com>
2025-08-08 17:32:52 +0100

    Add audience stats section with header and icon

--- src/features/profile/artist/components/artist-stats.tsx
+++ src/features/profile/artist/components/artist-stats.tsx
@@ -1,7 +1,28 @@
+import { audience } from "@/src/assets";
+import Image from "next/image";
+import ArtistAudienceStats from "./artist-audience-stats";
+
 const Stats = () => {
   return (
-    <section>
-      <h1>Stats View</h1>
+    <section className="flex w-full flex-col">
+      {/*Audience Stats*/}
+      <div className="flex w-full flex-col">
+        <div className="flex items-center gap-8">
+          <Image
+            src={audience}
+            alt="audience"
+            width={40}
+            height={40}
+            quality={100}
+            priority
+            className="object-center"
+          />
+          <h3 className="font-bricolage text-[20px] font-bold text-neutral-800 capitalize md:text-[24px]">
+            Your audience
+          </h3>
+        </div>
+        <ArtistAudienceStats />
+      </div>
     </section>
   );
 };
