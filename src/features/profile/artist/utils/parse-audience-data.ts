74ba1fcaf11bbc201701dcf53e9575cc14b9f1d9
3illBaby <thrillanon@outlook.com>
2025-08-08 17:32:32 +0100

    Add parse audience metrics utility function

---
+++ src/features/profile/artist/utils/parse-audience-data.ts
@@ -0,0 +1,62 @@
+import { IArtistMetricsDto } from "../../types/profile.types";
+import { ArtistError } from "../error/artist.error";
+
+export interface IParseAudienceMetrics {
+  metrics:
+    | IArtistMetricsDto
+    | ArtistError
+    | "An error occurred while fetching artist metrics"
+    | undefined;
+  metricsLoading: boolean;
+}
+
+export function parseAudienceMetrics(ctx: IParseAudienceMetrics) {
+  const { metrics, metricsLoading } = ctx;
+  const isValidMetrics =
+    metrics && typeof metrics === "object" && "data" in metrics;
+  const postViews = metricsLoading
+    ? 0
+    : isValidMetrics
+      ? (metrics.data?.postViews ?? 0)
+      : 0;
+  const followers = metricsLoading
+    ? 0
+    : isValidMetrics
+      ? (metrics.data?.followerCount ?? 0)
+      : 0;
+  const following = metricsLoading
+    ? 0
+    : isValidMetrics
+      ? (metrics.data?.followingCount ?? 0)
+      : 0;
+  const profileViews = metricsLoading
+    ? 0
+    : isValidMetrics
+      ? (metrics.data?.profileViews ?? 0)
+      : 0;
+
+  const AudienceData = [
+    {
+      id: 1,
+      context: "Followers",
+      value: followers,
+    },
+    {
+      id: 2,
+      context: "Post Views",
+      value: postViews,
+    },
+    {
+      id: 3,
+      context: "Following",
+      value: following,
+    },
+    {
+      id: 4,
+      context: "Profile Views",
+      value: profileViews,
+    },
+  ];
+
+  return AudienceData;
+}
