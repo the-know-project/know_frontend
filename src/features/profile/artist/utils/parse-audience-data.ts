12f57f9272c4acfc332723d2ee92d0428e4eaecb
3illBaby <thrillanon@outlook.com>
2025-08-23 20:00:12 +0100

    Add insight data parsing utility function

--- src/features/profile/artist/utils/parse-audience-data.ts
+++ src/features/profile/artist/utils/parse-audience-data.ts
@@ -60,3 +60,27 @@ export function parseAudienceMetrics(ctx: IParseAudienceMetrics) {
 
   return AudienceData;
 }
+
+export function parseInsightData(ctx: IParseAudienceMetrics) {
+  const { metrics, metricsLoading } = ctx;
+  const isValidMetrics =
+    metrics && typeof metrics === "object" && "data" in metrics;
+
+  const topInterestedBuyer = metricsLoading
+    ? 0
+    : isValidMetrics
+      ? (Number(metrics.data?.insight.topInterestedBuyersByPercentage) ??
+        50 / 100)
+      : 50 / 100;
+
+  const conversionRate = metricsLoading
+    ? 0
+    : isValidMetrics
+      ? (Number(metrics.data?.insight.conversionRateByPercentage) ?? 50 / 100)
+      : 50 / 100;
+
+  return {
+    topInterestedBuyer,
+    conversionRate,
+  };
+}
