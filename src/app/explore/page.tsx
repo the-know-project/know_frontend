5454ed3fe71864e1f6487ff22ec0b0cb65aaf679
3illBaby <thrillanon@outlook.com>
2025-08-25 18:06:08 +0100

    Remove async from explore page component

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,6 +1,6 @@
 import ExploreContainer from "@/src/features/explore/components/explore-container";
 
-const Page = async () => {
+const Page = () => {
   return (
     <section className="flex w-full flex-col px-6">
       <div className="mt-5 flex w-full flex-col gap-[50px]">
