b9afc7079d9c0a058ab29d9400ca076dde2d5516
3illBaby <thrillanon@outlook.com>
2025-08-30 00:11:41 +0100

    chore(metrics.types.ts): extended types

--- src/features/metrics/types/metrics.types.ts
+++ src/features/metrics/types/metrics.types.ts
@@ -1,8 +1,20 @@
 import { z } from "zod";
 import {
+  ArtistMonthlySalesDataResponse,
+  ArtistYearlySalesDataResponse,
+} from "../dto/metrics.dto";
+import {
   FetchSalesDataSchema,
   IncrementViewCountSchema,
+  SalesDurationSchema,
 } from "../schema/metrics.schema";
 
 export type IIncrementViewCount = z.infer<typeof IncrementViewCountSchema>;
 export type IFetchArtistSalesData = z.infer<typeof FetchSalesDataSchema>;
+export type ISalesDuration = z.infer<typeof SalesDurationSchema>;
+export type IArtistMonthlySalesDataResponse = z.infer<
+  typeof ArtistMonthlySalesDataResponse
+>;
+export type IArtistYearlySalesDataResponse = z.infer<
+  typeof ArtistYearlySalesDataResponse
+>;
