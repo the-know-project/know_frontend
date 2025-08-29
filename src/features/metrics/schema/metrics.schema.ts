2ad6d019849e4d8533b13ed7494f0bcd0f7677d9
3illBaby <thrillanon@outlook.com>
2025-08-29 23:40:42 +0100

    Add schema for fetching sales data

--- src/features/metrics/schema/metrics.schema.ts
+++ src/features/metrics/schema/metrics.schema.ts
@@ -4,3 +4,8 @@ export const IncrementViewCountSchema = z.object({
   userId: z.string().uuid(),
   fileId: z.string(),
 });
+
+export const FetchSalesDataSchema = z.object({
+  userId: z.string().uuid(),
+  duration: z.enum(["YEARLY", "MONTHLY"]).optional(),
+});
