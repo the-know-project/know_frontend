16b64daef49b5f64559750510cc9194365a1bee2
3illBaby <thrillanon@outlook.com>
2025-08-29 23:40:33 +0100

    Add user and sales metric endpoints

--- src/features/metrics/data/metrics.route.ts
+++ src/features/metrics/data/metrics.route.ts
@@ -1,3 +1,5 @@
 export enum METRICS_OP {
   INCREMENT_VIEW_COUNT = "/api/metrics/incrementPostViews",
+  USER_METRICS = "/api/metrics/userMetrics",
+  SALES_METRICS = "/api/metrics/salesMetrics",
 }
