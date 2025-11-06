a0a1cb6611cd667a0d1baf82cef8f68e1e40a753
Jaydbrown <jaiyeolawety705@gmail.com>
2025-11-06 08:52:52 -0800

    Remove AuthInitializer component

--- src/features/auth/components/AuthInitializer.tsx
+++ src/features/auth/components/AuthInitializer.tsx
@@ -1,17 +1,1 @@
-"use client";
 
-import { useEffect } from "react";
-import { useTokenStore } from "@/src/features/auth/store/useTokenStore";
-import { ApiClient } from "@/src/lib/api-client";
-
-export function AuthInitializer() {
-  const { accessToken, hasHydrated } = useTokenStore();
-
-  useEffect(() => {
-    if (hasHydrated && accessToken) {
-      ApiClient.setAuthToken(accessToken);
-    }
-  }, [hasHydrated, accessToken]);
-
-  return null;
-}
