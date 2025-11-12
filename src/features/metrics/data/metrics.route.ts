f0a7304bf9e24c7ee7ac055d1b13a801f58e6c14
3illBaby <thrillanon@outlook.com>
2025-11-12 22:51:06 +0100

    chore(metrics.data.ts): registered metrics endpoints

--- src/features/metrics/data/metrics.route.ts
+++ src/features/metrics/data/metrics.route.ts
@@ -2,4 +2,8 @@ export enum METRICS_OP {
   INCREMENT_VIEW_COUNT = "/api/metrics/incrementPostViews",
   USER_METRICS = "/api/metrics/userMetrics",
   SALES_METRICS = "/api/metrics/salesMetrics",
+  FOLLOW_USER = "/api/metrics/followUser",
+  UNFOLLOW_USER = "/api/metrics/unfollowUser",
+  USER_FOLLOWERS = "/api/metrics/fetchUserFollowers",
+  USER_FOLLOWING = "/api/metrics/fetchUserFollowing",
 }
