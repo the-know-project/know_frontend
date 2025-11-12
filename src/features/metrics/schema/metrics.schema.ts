644efeaa85fbfab3b71a65bfda0ee077363a4ead
3illBaby <thrillanon@outlook.com>
2025-11-12 22:54:23 +0100

    chore(metrics.schema.ts): added schema for follow operations

--- src/features/metrics/schema/metrics.schema.ts
+++ src/features/metrics/schema/metrics.schema.ts
@@ -11,3 +11,14 @@ export const FetchSalesDataSchema = z.object({
   userId: z.string().uuid(),
   duration: SalesDurationSchema,
 });
+
+export const FollowSchema = z.object({
+  followerId: z.string().uuid(),
+  followingId: z.string().uuid(),
+});
+
+export const FetchUserFollowersSchema = z.object({
+  userId: z.string().uuid(),
+  page: z.number().optional(),
+  limit: z.number().optional(),
+});
