b666b3092c682f952bc4b665052a845055ea3c25
3illBaby <thrillanon@outlook.com>
2025-11-12 09:58:21 +0100

    chore(layout.tsx): used dynamic import and removed unneccessary comment

--- src/app/buyer-profile/layout.tsx
+++ src/app/buyer-profile/layout.tsx
@@ -1,5 +1,4 @@
-// app/orders/layout.tsx
-import Sidebar from "../../features/profile/layout/buyer-sidebar";
+import Sidebar from "@/src/features/profile/layout/buyer-sidebar";
 import ExploreNav from "@/src/features/explore/components/explore-nav";
 
 export default function OrdersLayout({
@@ -9,10 +8,8 @@ export default function OrdersLayout({
 }) {
   return (
     <div className="flex min-h-screen flex-col">
-      {/*  Full-width header at the top */}
       <ExploreNav />
 
-      {/*  Sidebar + main content beneath */}
       <div className="mt-16 flex flex-1">
         <Sidebar />
         <main className="flex-1 p-4">{children}</main>
