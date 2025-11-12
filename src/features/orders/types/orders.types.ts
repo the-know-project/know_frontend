8ac9cdc6c4c1f500121d01df4e63035da88706ed
3illBaby <thrillanon@outlook.com>
2025-11-12 20:07:54 +0100

    chore(orders.types.ts): added types

--- src/features/orders/types/orders.types.ts
+++ src/features/orders/types/orders.types.ts
@@ -1,4 +1,16 @@
 import { z } from "zod";
-import { CreateOrderDto } from "../dto/orders.dto";
+import {
+  FetchUserOrdersDto,
+  CreateOrderDto,
+  FetchUserOrders,
+  OrderStatus,
+  OrdersSummaryDto,
+  OrdersByIdDto,
+} from "../dto/orders.dto";
 
 export type ICreateOrder = z.infer<typeof CreateOrderDto>;
+export type IFetchUserOrders = z.infer<typeof FetchUserOrders>;
+export type TOrderStatus = z.infer<typeof OrderStatus>;
+export type UserOrdersResponse = z.infer<typeof FetchUserOrdersDto>;
+export type OrderSummaryResponse = z.infer<typeof OrdersSummaryDto>;
+export type OrdersByIdResponse = z.infer<typeof OrdersByIdDto>;
