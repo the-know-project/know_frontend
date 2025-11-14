5ab2fb0914d85c99706365e788f07a2286f750f9
3illBaby <thrillanon@outlook.com>
2025-11-14 16:14:50 +0100

    fix(cart.route.ts): used correct remove from cart route

--- src/features/cart/data/cart.route.ts
+++ src/features/cart/data/cart.route.ts
@@ -1,5 +1,5 @@
 export enum CART_OP {
   ADD_TO_CART = "/api/cart/addToCart",
-  REMOVE_FROM_CART = "/api/cart/removeFromCart",
+  REMOVE_FROM_CART = "/api/cart/deleteFromCart",
   FETCH_USER_CART = "/api/cart/userCartItems",
 }
