1fff44443ae4bae7d8ac196a9cb2bdc5c379b7f8
3illBaby <thrillanon@outlook.com>
2025-11-17 19:00:19 +0100

    refactor(route.ts): moved route handler here and parse to form data

---
+++ src/features/profile/api/update-profile/route.ts
@@ -0,0 +1,15 @@
+import { ApiClient } from "@/src/lib/api-client";
+import { IUpdateProfileRequest } from "../../types/user.types";
+import { PROFILE_OP } from "../../data/profile.route";
+
+export async function updateProfile(ctx: IUpdateProfileRequest) {
+  const formData = new FormData();
+  Object.entries(ctx).forEach(([key, value]) => {
+    if (typeof File !== "undefined" && value instanceof File) {
+      formData.append(key, value);
+    } else if (value !== undefined && value !== null) {
+      formData.append(key, value as string);
+    }
+  });
+  return await ApiClient.put(PROFILE_OP.UPDATE_PROFILE, formData);
+}
