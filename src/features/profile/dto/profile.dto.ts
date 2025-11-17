c5f77b363f3ae25367ef547592687706702fe5e3
3illBaby <thrillanon@outlook.com>
2025-11-17 19:08:45 +0100

    chore(profile.dto.ts): added dto

--- src/features/profile/dto/profile.dto.ts
+++ src/features/profile/dto/profile.dto.ts
@@ -16,3 +16,20 @@ export const ArtistDetailsDtoSchema = z.object({
   data: ArtistDetailsDataSchema,
   meta: z.any().nullable(),
 });
+
+export const UpdateProfileSchema = z.object({
+  userId: z.string(),
+  email: z.string().email(),
+  description: z.string(),
+  name: z.string(),
+  profilePicture: z.instanceof(File),
+  username: z.string(),
+  password: z.string(),
+  oldPassword: z.string(),
+  newPassword: z.string(),
+  country: z.string(),
+  address: z.string(),
+  city: z.string(),
+  state: z.string(),
+  postalCode: z.string(),
+});
