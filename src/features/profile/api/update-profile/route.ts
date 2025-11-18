bddd5073a1093f9f46a0846a842d420f759a5cc3
3illBaby <thrillanon@outlook.com>
2025-11-18 20:38:49 +0100

    chore(route.ts): logged key and value being passed to backend server

--- src/features/profile/api/update-profile/route.ts
+++ src/features/profile/api/update-profile/route.ts
@@ -6,6 +6,7 @@ export async function updateProfile(ctx: IUpdateProfileRequest) {
   const formData = new FormData();
   Object.entries(ctx).forEach(([key, value]) => {
     if (typeof File !== "undefined" && (value as any) instanceof File) {
+      console.log(`key: ${key}, value: ${value}`);
       formData.append(key, value);
     } else if (value !== undefined && value !== null) {
       formData.append(key, value as string);
