7b3cb32bd2368b72997e058926a54721f64c7fa9
3illBaby <thrillanon@outlook.com>
2025-11-19 17:26:46 +0100

    refactor(use-login.tsx): refactored login hook

--- src/features/auth/hooks/use-login.tsx
+++ src/features/auth/hooks/use-login.tsx
@@ -2,8 +2,10 @@ import { handleAxiosError } from "@/src/utils/handle-axios-error";
 import { useMutation } from "@tanstack/react-query";
 import { login } from "../api/login/route";
 import { LoginResponseDto } from "../dto/auth.dto";
-import { ILogin, ILoginSuccess } from "../types/auth.types";
+import { ILogin, ILoginResponseDto, ILoginSuccess } from "../types/auth.types";
 import { useAuth } from "./use-auth";
+import { err, ok, ResultAsync } from "neverthrow";
+import { AuthError } from "../error/auth.error";
 
 export const useLogin = () => {
   const auth = useAuth();
@@ -11,25 +13,39 @@ export const useLogin = () => {
   return useMutation({
     mutationFn: async (ctx: ILogin): Promise<ILoginSuccess> => {
       try {
-        const data = await login(ctx);
-        console.log(data);
-        const validatedData = LoginResponseDto.parse(data);
-        if (validatedData.status !== 200 || !validatedData.data) {
-          throw new Error(validatedData.message || "Login Failed");
+        const response = await ResultAsync.fromPromise(
+          login(ctx),
+          (error) =>
+            new AuthError(`An error occurred while logging in ${error}`),
+        ).andThen((data) => {
+          if (data.status === 200) {
+            return ok(data);
+          } else {
+            return err(
+              new AuthError(
+                `An error occurred while logging in ${data.message}`,
+              ),
+            );
+          }
+        });
+
+        if (response.isErr()) {
+          throw response.error;
         }
 
-        const { accessToken, user } = validatedData.data;
+        const result = response.value as ILoginResponseDto;
+        const user = result.data.user;
 
         return {
-          status: validatedData.status,
-          message: validatedData.message,
+          status: result.status,
+          message: result.message,
           user: {
             id: user.id,
             email: user.email,
             firstName: user.firstName,
             imageUrl: user.imageUrl,
           },
-          accessToken,
+          accessToken: result.data.accessToken,
           role: user.role,
           isFirstTime: user.isFirstTime,
         };
