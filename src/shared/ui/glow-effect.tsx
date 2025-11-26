2028e2bb835bd4031688a17611468b31c46dafab
3illBaby <thrillanon@outlook.com>
2025-11-26 11:56:37 +0100

    fix glow effect

--- src/shared/ui/glow-effect.tsx
+++ src/shared/ui/glow-effect.tsx
@@ -40,7 +40,7 @@ export function GlowEffect({
   const BASE_TRANSITION = {
     repeat: Infinity,
     duration: duration,
-    ease: "linear",
+    ease: "linear" as const,
   };
 
   const animations = {
@@ -63,7 +63,7 @@ export function GlowEffect({
       transition: {
         ...(transition ?? {
           ...BASE_TRANSITION,
-          repeatType: "mirror",
+          repeatType: "mirror" as const,
         }),
       },
     },
@@ -78,7 +78,7 @@ export function GlowEffect({
       transition: {
         ...(transition ?? {
           ...BASE_TRANSITION,
-          repeatType: "mirror",
+          repeatType: "mirror" as const,
         }),
       },
     },
@@ -90,7 +90,7 @@ export function GlowEffect({
       transition: {
         ...(transition ?? {
           ...BASE_TRANSITION,
-          repeatType: "mirror",
+          repeatType: "mirror" as const,
         }),
       },
     },
@@ -102,7 +102,7 @@ export function GlowEffect({
       transition: {
         ...(transition ?? {
           ...BASE_TRANSITION,
-          repeatType: "mirror",
+          repeatType: "mirror" as const,
         }),
       },
     },
