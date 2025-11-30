3ad52a6ce429a3315aa5262592258a0b93a60238
3illBaby <thrillanon@outlook.com>
2025-11-30 21:39:50 +0100

    chore(metrics.dto.ts): added response dto

--- src/features/metrics/dto/metrics.dto.ts
+++ src/features/metrics/dto/metrics.dto.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+import { Pagination } from "../../explore/dto/explore.dto";
 
 export const MonthlySalesData = z.object({
   salesValue: z.string(),
@@ -8,6 +9,15 @@ export const MonthlySalesData = z.object({
 export const YearlySalesData = z.object({
   salesValue: z.string(),
   YearlySalesData: z.string(),
+});
+
+export const FollowingData = z.object({
+  id: z.string().uuid(),
+  username: z.string(),
+  firstName: z.string(),
+  lastName: z.string(),
+  imageUrl: z.string().url().optional(),
+  role: z.string(),
 });
 
 export const ArtistMonthlySalesDataResponse = z.object({
@@ -20,4 +30,11 @@ export const ArtistYearlySalesDataResponse = z.object({
   status: z.number(),
   message: z.string(),
   data: z.array(YearlySalesData),
+});
+
+export const FetchUserFollowingResponse = z.object({
+  status: z.number(),
+  message: z.string(),
+  data: z.array(FollowingData),
+  meta: Pagination,
 });
