c4f5f8e291abe66c79ebb1105d1ea4338d7ce820
3illBaby <thrillanon@outlook.com>
2025-11-30 23:54:19 +0100

    moved to folder

---
+++ src/features/metrics/api/incr-view-count/route.ts
@@ -0,0 +1,7 @@
+import { ApiClient } from "@/src/lib/api-client";
+import { IIncrementViewCount } from "../../types/metrics.types";
+import { METRICS_OP } from "../../data/metrics.route";
+
+export async function incrementViewCount(ctx: IIncrementViewCount) {
+  return await ApiClient.post(METRICS_OP.INCREMENT_VIEW_COUNT, ctx);
+}
