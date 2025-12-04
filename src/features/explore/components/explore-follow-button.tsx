c160638842acf011b578e37e66c49e23a0961546
3illBaby <thrillanon@outlook.com>
2025-12-04 02:00:30 +0100

    feat(explore-follow-button): add logic to component

--- src/features/explore/components/explore-follow-button.tsx
+++ src/features/explore/components/explore-follow-button.tsx
@@ -1,19 +1,41 @@
+"use client";
+
 import { Button } from "@/src/shared/ui/button";
 import { PlusCircle } from "lucide-react";
+import { useFollowUser } from "../../metrics/hooks/use-follow-user";
+import { useUnfollowUser } from "../../metrics/hooks/use-unfollow-user";
+import { useFollowActions } from "../../metrics/state/store/metrics.store";
 
 interface ExploreFollowButtonProps {
   artistId: string;
 }
 
 const ExploreFollowButton = ({ artistId }: ExploreFollowButtonProps) => {
-  const handleFollow = () => {
-    console.log("Following artist:", artistId);
-    // TODO: Add logic to follow the artist
+  const { mutateAsync: followUser, isPending } = useFollowUser();
+  const { mutateAsync: unFollowUser, isPending: isUnfollowing } =
+    useUnfollowUser();
+  const { useIsUserFollowing } = useFollowActions();
+
+  const isFollowing = useIsUserFollowing(artistId);
+
+  const handleFollowUser = async () => {
+    if (isFollowing) {
+      await unFollowUser({
+        followingId: artistId,
+      });
+    } else {
+      await followUser({
+        followingId: artistId,
+      });
+    }
   };
 
   return (
-    <Button className="font-bebas w-full cursor-pointer bg-[#1E3A8A] p-6 tracking-wider hover:bg-[#2c52ba]">
-      <PlusCircle /> Follow
+    <Button
+      onClick={handleFollowUser}
+      className="font-bebas w-full cursor-pointer bg-[#1E3A8A] p-6 tracking-wider hover:bg-[#2c52ba]"
+    >
+      <PlusCircle /> {isFollowing === true ? "Unfollow" : "Follow"}
     </Button>
   );
 };
