227504f24ffa78de08e88d031065d5a7f3973c80
3illBaby <thrillanon@outlook.com>
2025-12-12 23:50:15 +0100

    fix(route.ts): passed correct sig

--- src/features/explore/api/hide-post-comment/route.ts
+++ src/features/explore/api/hide-post-comment/route.ts
@@ -1,6 +1,7 @@
 import { ApiClient } from "@/src/lib/api-client";
 import { EXPLORE_OP } from "../../data/explore.route";
+import { IHideComment } from "../../types/explore-comment.types";
 
-export async function hidePostComment(commentId: string) {
-  return await ApiClient.patch(EXPLORE_OP.HIDE_POST_COMMENT, { commentId });
+export async function hidePostComment(ctx: Omit<IHideComment, "fileId">) {
+  return await ApiClient.patch(EXPLORE_OP.HIDE_POST_COMMENT, ctx);
 }
