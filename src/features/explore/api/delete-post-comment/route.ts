a913576505e316d9ad209d20c9a165baecf03511
3illBaby <thrillanon@outlook.com>
2025-12-12 23:48:14 +0100

    fix(route.ts): passed correct sigf

--- src/features/explore/api/delete-post-comment/route.ts
+++ src/features/explore/api/delete-post-comment/route.ts
@@ -1,8 +1,7 @@
 import { ApiClient } from "@/src/lib/api-client";
 import { EXPLORE_OP } from "../../data/explore.route";
+import { IDeleteComment } from "../../types/explore-comment.types";
 
-export async function deletePostComment(commentId: string) {
-  return await ApiClient.delete(EXPLORE_OP.DELETE_POST_COMMENT, { 
-    data: { commentId } 
-  });
+export async function deletePostComment(ctx: IDeleteComment) {
+  return await ApiClient.delete(EXPLORE_OP.DELETE_POST_COMMENT, ctx);
 }
