b21606132143de9ffe1c110ed405969dba4286a0
3illBaby <thrillanon@outlook.com>
2025-12-12 23:53:41 +0100

    chore(explore-comment.types.ts): extended

--- src/features/explore/types/explore-comment.types.ts
+++ src/features/explore/types/explore-comment.types.ts
@@ -1,29 +1,33 @@
-import { z } from 'zod';
-import { exploreComment, meta, comments } from '../dto/explore-comment.dto';
+import { z } from "zod";
+import { exploreComment, meta, comments } from "../dto/explore-comment.dto";
 
 export type TExploreComment = z.infer<typeof exploreComment>;
 export type TMeta = z.infer<typeof meta>;
 export type TComments = z.infer<typeof comments>;
+export type IPostComment = z.infer<typeof AddCommentPayload>;
+export type IDeleteComment = z.infer<typeof DeleteCommentPayload>;
+export type IHideComment = z.infer<typeof DeleteCommentPayload>;
 
+export const AddCommentPayload = z.object({
+  userId: z.string(),
+  fileId: z.string(),
+  comment: z.string(),
+});
 
-export type AddCommentPayload = {
-  postId: string;
-  comment: string;
-  fileId?: string;
-};
-
-export type DeleteCommentPayload = {
-  commentId: string;
-  postId: string;
-};
-
-export type HideCommentPayload = {
-  commentId: string;
-  postId: string;
-};
+export const DeleteCommentPayload = z.object({
+  fileId: z.string().optional(),
+  userId: z.string(),
+  commentId: z.string(),
+});
 
 export type UpdateCommentPayload = {
   commentId: string;
   postId: string;
   content: string;
 };
+
+export interface IFetchPostComments {
+  fileId: string;
+  page?: number;
+  limit?: number;
+}
