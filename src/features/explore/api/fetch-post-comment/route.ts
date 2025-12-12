79f6ff4f729fce485c1c21e5a56d5628786a1e3b
3illBaby <thrillanon@outlook.com>
2025-12-12 23:48:33 +0100

    fix(route.ts): passed correct sig

--- src/features/explore/api/fetch-post-comment/route.ts
+++ src/features/explore/api/fetch-post-comment/route.ts
@@ -1,7 +1,9 @@
 import { ApiClient } from "@/src/lib/api-client";
 import { EXPLORE_OP } from "../../data/explore.route";
+import { IFetchPostComments } from "../../types/explore-comment.types";
 
-export async function fetchPostComments(postId: string, page: number = 1) {
-  const path = `${EXPLORE_OP.FETCH_POST_COMMENTS}?postId=${postId}&page=${page}`;
+export async function fetchPostComments(ctx: IFetchPostComments) {
+  const { fileId, page = 1, limit = 10 } = ctx;
+  const path = `${EXPLORE_OP.FETCH_POST_COMMENTS}?fileId=${fileId}&page=${page}&limit=${limit}`;
   return await ApiClient.get(path);
 }
