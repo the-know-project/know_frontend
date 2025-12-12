069841862a080041e9d8396af9126b355df87f27
3illBaby <thrillanon@outlook.com>
2025-12-12 23:51:50 +0100

    chore(explore-comment.dto): fixed wrong key

--- src/features/explore/dto/explore-comment.dto.ts
+++ src/features/explore/dto/explore-comment.dto.ts
@@ -1,27 +1,28 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 export const exploreComment = z.object({
-    id: z.string(),
-    userId: z.string().uuid(),
-    firstName: z.string(),
-    lastName: z.string(),
-    ProfilePicture: z.string().nullable(),
-    comment: z.string(),
-    createdAt: z.number(),
-    fileId: z.string().optional(),
+  id: z.string(),
+  userId: z.string().uuid(),
+  firstName: z.string(),
+  lastName: z.string(),
+  profilePicture: z.string().nullable(),
+  comment: z.string(),
+  createdAt: z.number(),
+  fileId: z.string().optional(),
 });
 
 export const meta = z.object({
-    currentPage: z.number().min(1),
-    totalPage: z.number().min(1),
-    totalItems: z.number().min(1),
-    itemsPerPage: z.number().min(10),
-    hasNextPage: z.boolean(),
-    hasPreviousPage: z.boolean(), 
+  currentPage: z.number().min(1),
+  totalPages: z.number().min(1),
+  totalItems: z.number().min(1),
+  itemsPerPage: z.number().min(10),
+  hasNextPage: z.boolean(),
+  hasPreviousPage: z.boolean(),
 });
 
 export const comments = z.object({
-    status: z.number(),
-    message: z.string(),
-    data: z.array(z.union([exploreComment, meta])),
+  status: z.number(),
+  message: z.string(),
+  data: z.array(exploreComment),
+  meta: meta,
 });
