519ebd3370c259984698ce19e5e3290612cbe248
3illBaby <thrillanon@outlook.com>
2025-12-16 16:56:19 +0100

    chore(artist-collection.tsx): init artist collection overview tab

--- src/features/profile/artist/components/artist-collection.tsx
+++ src/features/profile/artist/components/artist-collection.tsx
@@ -1,9 +1,11 @@
 import ArtistCollectionNav from "./artist-collection-nav";
+import Collections from "./collections";
 
 const ArtistCollection = () => {
   return (
-    <section className="relative flex min-h-screen w-full flex-col">
-      <div className="bottom fixed">
+    <section className="relative -mt-[50px] flex w-full flex-col">
+      <Collections />
+      <div className="sticky bottom-0 z-50 self-center px-4 lg:hidden">
         <ArtistCollectionNav />
       </div>
     </section>
