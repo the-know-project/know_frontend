178f89f6d820c0fe5b628bfd768b9ebc99c0881e
3illBaby <thrillanon@outlook.com>
2025-12-16 17:14:59 +0100

    chore(collection.route.ts): registered collection route

---
+++ src/features/profile/data/collection.route.ts
@@ -0,0 +1,11 @@
+export enum COLLECTION_OP {
+  FETCH_ALL_COLLECTIONS = "api/collections/fetchAllCollections",
+  CREATE_COLLECTION = "api/collections/createCollection",
+  ADD_TO_COLLECTION = "api/collections/addToCollection",
+  REMOVE_FROM_COLLECTION = "api/collections/removeFromCollection",
+  UPDATE_COLLECTION = "api/collections/updateCollection",
+  DELETE_COLLECTION = "api/collections/deleteCollection",
+  FETCH_COLLECTION = "api/collections/fetchCollection",
+  LIKE_COLLECTION = "api/collections/likeCollection",
+  UNLIKE_COLLECTION = "api/collections/unlikeCollection",
+}
