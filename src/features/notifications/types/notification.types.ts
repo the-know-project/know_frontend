e7c4703a4734fb9282c6ab0a71479f490af120fb
3illBaby <thrillanon@outlook.com>
2025-12-19 22:26:36 +0100

    chore(notification.types.ts): updated types

--- src/features/notifications/types/notification.types.ts
+++ src/features/notifications/types/notification.types.ts
@@ -1,6 +1,10 @@
 import { z } from "zod";
-import { NotificationData } from "../dto/notifications.dto";
+import {
+  NotificationData,
+  NotificationResponseDto,
+} from "../dto/notifications.dto";
 import { DeleteNotifications } from "../schema/notification.schema";
 
 export type INotificationData = z.infer<typeof NotificationData>;
 export type IDeleteNotifications = z.infer<typeof DeleteNotifications>;
+export type INotificationResponseDto = z.infer<typeof NotificationResponseDto>;
