ce8d79ed09eed6c78935914271ec3d529540e513
3illBaby <thrillanon@outlook.com>
2025-12-19 20:22:45 +0100

    chore(notifications.interface.ts): added notification state

---
+++ src/features/notifications/state/interface/notifications.interface.ts
@@ -0,0 +1,16 @@
+export interface INotification {
+  id: string | number;
+  image: string;
+  secondaryImage?: string | null;
+  content: string;
+  createdAt: number;
+}
+
+export interface INotificationState {
+  userNotification: Record<string, INotification[]>;
+  _hasHydrated: boolean;
+  setHasHydrated: (state: boolean) => void;
+  fetchNotifications: (userId: string) => INotification[];
+  deleteNotification: (userId: string, id: string) => void;
+  deleteAllNotifications: (userId: string) => void;
+}
