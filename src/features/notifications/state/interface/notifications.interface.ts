0167ce0bf648a91983aeb0d50fd28dfc9eefad6d
3illBaby <thrillanon@outlook.com>
2025-12-19 20:28:31 +0100

    chore(notifications.interface.ts): created notification store interface

--- src/features/notifications/state/interface/notifications.interface.ts
+++ src/features/notifications/state/interface/notifications.interface.ts
@@ -10,7 +10,10 @@ export interface INotificationState {
   userNotification: Record<string, INotification[]>;
   _hasHydrated: boolean;
   setHasHydrated: (state: boolean) => void;
+
+  addNotifications: (userId: string, notifications: INotification[]) => void;
   fetchNotifications: (userId: string) => INotification[];
+
   deleteNotification: (userId: string, id: string) => void;
   deleteAllNotifications: (userId: string) => void;
 }
