9b6bd4fe473510fd973f5ce278cef930e9f19d03
3illBaby <thrillanon@outlook.com>
2025-12-20 10:50:30 +0100

    chore(route.ts): made path constant

--- src/features/profile/artist/api/posts/route.ts
+++ src/features/profile/artist/api/posts/route.ts
@@ -3,7 +3,7 @@ import { PROFILE_OP } from "../../../data/profile.route";
 import { IFetchUserAsset } from "../../../types/profile.types";
 
 export async function fetchUserPosts(ctx: IFetchUserAsset) {
-  let path = `${PROFILE_OP.USER_ASSETS}?userId=${ctx.userId}&page=${ctx.page || 1}&limit=${ctx.limit || 12}`;
+  const path = `${PROFILE_OP.USER_ASSETS}?userId=${ctx.userId}&page=${ctx.page || 1}&limit=${ctx.limit || 12}`;
 
   return ApiClient.get(path);
 }
