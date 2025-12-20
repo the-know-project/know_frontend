6748022a83cc5e915c4f5d943ef6851a2a5ed8f9
3illBaby <thrillanon@outlook.com>
2025-12-20 16:12:33 +0100

    refactor(collections.tsx): consumed collections as props

--- src/features/profile/artist/components/collections.tsx
+++ src/features/profile/artist/components/collections.tsx
@@ -1,21 +1,25 @@
-import { CollectionData } from "@/src/constants/constants";
 import CollectionCard from "@/src/shared/ui/collection-card";
-
-const Collections = () => {
+import { TCollectionData } from "../types/collections.types";
+interface ICollections {
+  collections: TCollectionData[];
+}
+const Collections: React.FC<ICollections> = ({ collections }) => {
   return (
-    <section className="grid grid-cols-1 gap-5 space-y-[10px] md:grid-cols-2">
-      {CollectionData.map((_collection) => (
-        <div key={_collection.id} className="w-full">
-          <CollectionCard
-            id={_collection.id}
-            firstName={_collection.firstName}
-            lastName={_collection.lastName}
-            src={_collection.src}
-            numOfArt={_collection.numOfArt}
-            title={_collection.title}
-          />
-        </div>
-      ))}
+    <section className="flex w-full flex-col">
+      <div className="grid grid-cols-1 gap-5 space-y-[10px] md:grid-cols-2">
+        {collections.map((_collection) => (
+          <div key={_collection.id} className="w-full">
+            <CollectionCard
+              id={_collection.id}
+              firstName={_collection.firstName}
+              lastName={_collection.lastName}
+              src={_collection.bannerUrl}
+              numOfArt={_collection.numOfArt}
+              title={_collection.title}
+            />
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
