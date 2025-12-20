6caf8e94848f00e03956b70a19cacf3168f069cd
3illBaby <thrillanon@outlook.com>
2025-12-20 10:34:07 +0100

    style(notification-card,tsx): added animations

--- src/features/notifications/components/notification-card.tsx
+++ src/features/notifications/components/notification-card.tsx
@@ -5,6 +5,7 @@ import { IconChecks, IconX } from "@tabler/icons-react";
 import Image from "next/image";
 import { useDeleteUserNotifications } from "../hooks/use-delete-user-notifications";
 import { showLog } from "@/src/utils/logger";
+import { AnimatePresence, motion } from "framer-motion";
 
 interface NotificationProps {
   id: string | number;
@@ -43,6 +44,11 @@ const NotificationCard: React.FC<INotificationCard> = ({ data }) => {
     }
   };
 
+  const variants = {
+    hidden: { opacity: 0, y: -20 },
+    visible: { opacity: 1, y: 0 },
+  };
+
   return (
     <section className="scrollbar-hide relative flex max-h-[600px] min-h-[300px] min-w-[350px] scroll-m-2 flex-col overflow-auto scroll-smooth rounded-[15px] border border-white/20 bg-white px-2 py-4 opacity-95 shadow-[0_8px_32px_0_rgba(31,38,135,0.37),inset_0_1px_0_0_rgba(255,255,255,0.18)]">
       <div className="mb-5 flex w-full flex-col items-end justify-end">
@@ -67,58 +73,68 @@ const NotificationCard: React.FC<INotificationCard> = ({ data }) => {
           </h3>
         )}
 
-        {data.length > 0 &&
-          data.map((notification: NotificationProps, index) => (
-            <div className="flex w-full flex-col gap-2" key={notification.id}>
-              <div
-                className="motion-preset-blur-down motion-duration-700 flex items-center gap-5"
-                style={{
-                  animationDelay: `${index * 100}ms`,
-                }}
+        <AnimatePresence>
+          {data.length > 0 &&
+            data.map((notification: NotificationProps, index) => (
+              <motion.div
+                key={index}
+                variants={variants}
+                initial="hidden"
+                animate="visible"
+                exit="hidden"
+                className="flex w-full flex-col gap-2"
+                key={notification.id}
               >
-                <div className="flex w-fit items-center justify-center rounded-full bg-gray-200 mask-auto p-2">
-                  <Image
-                    src={notification.image}
-                    alt={`notification_image`}
-                    width={30}
-                    height={30}
-                    className="rounded-full"
-                  />
-                </div>
-
-                <div className="flex flex-1 flex-col gap-1 capitalize">
-                  <h3 className="font-bricolage text-[12px] font-bold sm:text-sm">
-                    {notification.content}
-                  </h3>
-                  <p className="font-grotesk text-[9px] text-gray-400 sm:text-xs">
-                    {formatTimestampToReadable(notification.createdAt)}
-                  </p>
-                </div>
-
-                {notification.secondaryImage && (
-                  <Image
-                    src={notification.secondaryImage}
-                    alt={`notification_image`}
-                    width={30}
-                    height={30}
-                    className="rounded-full"
-                  />
-                )}
-
-                <button
-                  onClick={() => handleDeleteSingle(String(notification.id))}
-                  className="group flex h-6 w-6 items-center justify-center rounded-full bg-gray-100 hover:bg-red-100 disabled:opacity-50"
+                <div
+                  className="motion-preset-blur-down motion-duration-700 flex items-center gap-5"
+                  style={{
+                    animationDelay: `${index * 100}ms`,
+                  }}
                 >
-                  <IconX
-                    width={12}
-                    height={12}
-                    className="text-gray-400 group-hover:text-red-500"
-                  />
-                </button>
-              </div>
-              <hr className="w-full border-t border-gray-200" />
-            </div>
-          ))}
+                  <div className="flex w-fit items-center justify-center rounded-full bg-gray-200 mask-auto p-2">
+                    <Image
+                      src={notification.image}
+                      alt={`notification_image`}
+                      width={30}
+                      height={30}
+                      className="rounded-full"
+                    />
+                  </div>
+
+                  <div className="flex flex-1 flex-col gap-1 capitalize">
+                    <h3 className="font-bricolage text-[12px] font-bold sm:text-sm">
+                      {notification.content}
+                    </h3>
+                    <p className="font-grotesk text-[9px] text-gray-400 sm:text-xs">
+                      {formatTimestampToReadable(notification.createdAt)}
+                    </p>
+                  </div>
+
+                  {notification.secondaryImage && (
+                    <Image
+                      src={notification.secondaryImage}
+                      alt={`notification_image`}
+                      width={30}
+                      height={30}
+                      className="rounded-full"
+                    />
+                  )}
+
+                  <button
+                    onClick={() => handleDeleteSingle(String(notification.id))}
+                    className="group flex h-6 w-6 items-center justify-center rounded-full bg-gray-100 hover:bg-red-100 disabled:opacity-50"
+                  >
+                    <IconX
+                      width={12}
+                      height={12}
+                      className="text-gray-400 group-hover:text-red-500"
+                    />
+                  </button>
+                </div>
+                <hr className="w-full border-t border-gray-200" />
+              </motion.div>
+            ))}
+        </AnimatePresence>
       </div>
     </section>
   );
