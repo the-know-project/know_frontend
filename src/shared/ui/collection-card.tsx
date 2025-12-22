f348035ba6c6b3976f1c5801879261cb63ae8955
3illBaby <thrillanon@outlook.com>
2025-12-22 23:25:46 +0100

    style(collection-card.tsx): update background card

--- src/shared/ui/collection-card.tsx
+++ src/shared/ui/collection-card.tsx
@@ -38,13 +38,11 @@ const CollectionCard: React.FC<ICollectionCard> = ({
           style={{
             background:
               "linear-gradient(135deg, rgba(99,102,241,0.35), rgba(236,72,153,0.35))",
-            filter: "blur(10px)",
             transformOrigin: "center",
           }}
           animate={{
-            scale: 0.95,
-            y: -5,
-            rotate: -40,
+            scale: 0.98,
+            y: -60,
           }}
           transition={{
             type: "spring",
@@ -58,13 +56,12 @@ const CollectionCard: React.FC<ICollectionCard> = ({
           className="absolute inset-0 rounded-3xl"
           style={{
             background: "linear-gradient(135deg, rgba(59,130,246,0.45)",
-            filter: "blur(10px)",
+
             transformOrigin: "center",
           }}
           animate={{
             scale: 0.98,
-            y: -5,
-            rotate: 30,
+            y: -30,
           }}
           transition={{
             type: "spring",
@@ -91,17 +88,17 @@ const CollectionCard: React.FC<ICollectionCard> = ({
             className="h-[300px] w-full object-cover"
           />
         </motion.div>
-        <div className="absolute top-2 left-2 z-30 rounded-3xl bg-transparent p-2 shadow-lg backdrop-blur-2xl">
+        <div className="absolute top-2 left-2 z-30 rounded-3xl bg-neutral-500/10 p-2 shadow-lg backdrop-blur-2xl">
           <Settings2Icon width={24} height={25} className="text-white" />
         </div>
 
-        <div className="absolute top-2 right-2 z-30 rounded-3xl bg-transparent p-2 shadow-lg backdrop-blur-2xl">
+        <div className="absolute top-2 right-2 z-30 rounded-3xl bg-neutral-500/10 p-2 shadow-lg backdrop-blur-2xl">
           <p className="font-bebas flex self-center text-center font-bold tracking-wider text-white">
             {numOfArt}
           </p>
         </div>
 
-        <div className="group absolute bottom-7 z-30 w-full max-w-sm self-center rounded-3xl px-4 py-2 shadow-lg backdrop-blur-2xl md:max-w-md">
+        <div className="group absolute bottom-7 z-30 w-full max-w-sm self-center rounded-3xl bg-neutral-500/10 px-4 py-2 shadow-lg backdrop-blur-2xl md:max-w-md">
           <div className="flex flex-row items-center justify-between">
             <div className="flex flex-col items-start">
               <h3 className="font-bricolage text-sm font-bold text-white capitalize">
@@ -112,12 +109,12 @@ const CollectionCard: React.FC<ICollectionCard> = ({
               </p>
             </div>
 
-            <div className="rounded-3xl border border-neutral-700 bg-neutral-700 p-2 shadow-lg backdrop-blur-2xl">
+            <div className="group rounded-3xl border border-neutral-700 bg-neutral-700 p-2 shadow-lg backdrop-blur-2xl">
               <IconArrowRight
                 onClick={handleRoute}
                 width={24}
                 height={24}
-                className="text-white"
+                className="hover-scale-105 text-white transition-all duration-300 hover:-rotate-45 active:scale-95"
               />
             </div>
           </div>
