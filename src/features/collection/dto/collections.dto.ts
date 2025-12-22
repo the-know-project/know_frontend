58b36d902a6311360adb6f011aa2a1c33f70ab7b
3illBaby <thrillanon@outlook.com>
2025-12-22 20:10:28 +0100

    chore(collections.dto.ts): def fetch collection dto response

--- src/features/collection/dto/collections.dto.ts
+++ src/features/collection/dto/collections.dto.ts
@@ -35,3 +35,47 @@ export const FetchCollectionDto = z.object({
   userId: z.string().uuid(),
   collectionId: z.string().uuid(),
 });
+
+export const FetchCollectionAssetData = z.object({
+  fileId: z.string(),
+  assetUrl: z.string(),
+  artTitle: z.string(),
+  artDescription: z.string(),
+  artCategories: z.array(z.string()).min(1),
+  artTags: z.array(z.string()).optional(),
+  artSize: z.object({
+    width: z.number(),
+    height: z.number(),
+    weight: z.number(),
+    depth: z.number().optional(),
+    diameter: z.number().optional(),
+    length: z.number().optional(),
+    weightUnit: z.string().optional(),
+    dimensionUnit: z.string().optional(),
+  }),
+  isArtListed: z.boolean(),
+  isArtLocked: z.boolean(),
+  numOfLikes: z.number().min(0),
+  artistProfileUrl: z.string(),
+  artCreatedAt: z.date(),
+});
+
+export const FetchCollectionData = z.object({
+  id: z.string().uuid(),
+  title: z.string(),
+  description: z.string().optional(),
+  price: z.string(),
+  tags: z.array(z.string()).optional(),
+  numOfLikes: z.number().min(0),
+  currency: z.string(),
+  imageUrl: z.string().optional(),
+  bannerUrl: z.string(),
+  profileUrl: z.string(),
+  assetData: z.array(FetchCollectionAssetData).optional(),
+});
+
+export const FetchCollectionResponseDto = z.object({
+  status: z.number(),
+  message: z.string(),
+  data: FetchCollectionData,
+});
