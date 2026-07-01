c6f5222e029fb44bff29389b6952b0b862552903
3illBaby <thrillanon@outlook.com>
2026-07-01 10:59:09 +0100

    chore(personalize.types.ts): updated types

--- src/features/personalize/types/personalize.types.ts
+++ src/features/personalize/types/personalize.types.ts
@@ -1,4 +1,8 @@
 import { z } from "zod";
 import { PersonalizeSchema } from "../schema/personalize.schema";
+import { GetCategoriesResponseDto } from "../dto/personalize.dto";
 
 export type IPersonalize = z.infer<typeof PersonalizeSchema>;
+export type IGetCategoriesResponseDto = z.infer<
+  typeof GetCategoriesResponseDto
+>;
