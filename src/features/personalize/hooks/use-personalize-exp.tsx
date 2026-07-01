68e5c2f2bc82bbeeacd624a6d4fa9ffb7af610da
3illBaby <thrillanon@outlook.com>
2026-07-01 10:58:18 +0100

    refactor(use-personalize-exp.tsx): used never throw

--- src/features/personalize/hooks/use-personalize-exp.tsx
+++ src/features/personalize/hooks/use-personalize-exp.tsx
@@ -4,6 +4,9 @@ import { useTokenStore } from "../../auth/state/store";
 import { personalizeExperience } from "../api/prefrences/route";
 import { PersonalizeResponseDto } from "../dto/personalize.dto";
 import { selectUser } from "../../auth/state/selectors/token.selectors";
+import { err, ok, ResultAsync } from "neverthrow";
+import { PersonalizeError } from "../errors/personalize.error";
+import { IPersonalize } from "../types/personalize.types";
 
 export const usePersonalizeExp = () => {
   const user = useTokenStore(selectUser);
@@ -20,13 +23,26 @@ export const usePersonalizeExp = () => {
         throw new Error("User not authenticated");
       }
 
-      const result = await personalizeExperience({
-        userId: user.id,
-        categories: data,
+      const result = await ResultAsync.fromPromise(
+        personalizeExperience({
+          categories: data,
+        }),
+        (error) =>
+          new PersonalizeError(`Error personalizing experience: ${error}`),
+      ).andThen((data) => {
+        if (data.status === 200) {
+          const parsed = PersonalizeResponseDto.parse(data);
+          return ok(parsed);
+        } else {
+          return err(new PersonalizeError("Failed to personalize experience"));
+        }
       });
 
-      const validatedData = PersonalizeResponseDto.parse(result);
-      return validatedData;
+      if (result.isErr()) {
+        throw result.error;
+      }
+
+      return result.value;
     },
     onSuccess: () => {
       console.log("Personalization successful");
