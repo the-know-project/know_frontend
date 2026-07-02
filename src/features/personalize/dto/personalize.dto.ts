39f340af31a29c0effc06914d46bb384415f64a8
3illBaby <thrillanon@outlook.com>
2026-07-02 16:11:57 +0100

    chore(personalize.dto.ts): updated dto

--- src/features/personalize/dto/personalize.dto.ts
+++ src/features/personalize/dto/personalize.dto.ts
@@ -3,14 +3,7 @@ import { z } from "zod";
 export const GetCategoriesResponseDto = z.object({
   status: z.number(),
   message: z.string().min(1).max(255),
-  data: z.array(z.string().min(1).max(255)),
-  meta: z
-    .object({
-      total: z.number().min(0),
-      page: z.number().min(1),
-      limit: z.number().min(1).max(100),
-    })
-    .nullable(),
+  data: z.array(z.string()),
 });
 
 export const PersonalizeResponseDto = z.object({
