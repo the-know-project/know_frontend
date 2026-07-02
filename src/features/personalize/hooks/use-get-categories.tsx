00acda87e40d6131ecd39241d0b39e96bb7eec5b
3illBaby <thrillanon@outlook.com>
2026-07-02 16:13:30 +0100

    refactor(use-get-categories.tsx): refactored hook

--- src/features/personalize/hooks/use-get-categories.tsx
+++ src/features/personalize/hooks/use-get-categories.tsx
@@ -5,35 +5,29 @@ import { PersonalizeErrorMessages } from "../data/personalize.data";
 import { GetCategoriesResponseDto } from "../dto/personalize.dto";
 import { IGetCategoriesResponseDto } from "../types/personalize.types";
 
-export const getCategoriesQueryOptions = {
-  queryKey: ["get-categories"],
-  queryFn: async () => {
-    const result = await ResultAsync.fromPromise(
-      getCategories(),
-      (error) => new Error(`Personalize error: ${error}`),
-    ).andThen((data) => {
-      if (data.status === 200) {
-        try {
+export const useGetCategories = () => {
+  return useQuery({
+    queryKey: ["get-categories"],
+    queryFn: async () => {
+      const result = await ResultAsync.fromPromise(
+        getCategories(),
+        (error) => new Error(`Personalize error: ${error}`),
+      ).andThen((data) => {
+        if (data.status === 200) {
           const parsed = GetCategoriesResponseDto.parse(data);
           return ok(parsed);
-        } catch (e) {
-          return err(new Error(`Validation error: ${e}`));
+        } else {
+          return err(
+            new Error(PersonalizeErrorMessages.FAILED_TO_FETCH_CATEGORIES),
+          );
         }
-      } else {
-        return err(
-          new Error(PersonalizeErrorMessages.FAILED_TO_FETCH_CATEGORIES),
-        );
+      });
+
+      if (result.isErr()) {
+        throw result.error;
       }
-    });
 
-    if (result.isErr()) {
-      throw result.error;
-    }
-
-    return result.value as IGetCategoriesResponseDto;
-  },
-};
-
-export const useGetCategories = () => {
-  return useQuery(getCategoriesQueryOptions);
+      return result.value as IGetCategoriesResponseDto;
+    },
+  });
 };
