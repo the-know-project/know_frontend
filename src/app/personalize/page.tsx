0ef0a725964cca6172d1eb50bf6a3504719ef042
3illBaby <thrillanon@outlook.com>
2026-07-02 16:22:17 +0100

    chore(page.tsx): remove prefetching

--- src/app/personalize/page.tsx
+++ src/app/personalize/page.tsx
@@ -1,18 +1,10 @@
 import ArtSelection from "@/src/features/personalize/components/art-selection";
-import { getCategoriesQueryOptions } from "@/src/features/personalize/hooks/use-get-categories";
-
 import { TitleText } from "@/src/shared/layout/header";
-import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClient,
-} from "@tanstack/react-query";
+
 import Image from "next/image";
 
 const Page = async () => {
-  const queryClient = new QueryClient();
   try {
-    await queryClient.prefetchQuery(getCategoriesQueryOptions);
     return (
       <section className="relative z-50 flex w-full flex-col">
         <Image
@@ -35,9 +27,7 @@ const Page = async () => {
         </div>
 
         <div className="mt-[50px] flex w-full flex-col items-center justify-center">
-          <HydrationBoundary state={dehydrate(queryClient)}>
-            <ArtSelection />
-          </HydrationBoundary>
+          <ArtSelection />
         </div>
       </section>
     );
