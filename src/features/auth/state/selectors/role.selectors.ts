f866d48b7442b65ea5f302359f142534f1a00c90
3illBaby <thrillanon@outlook.com>
2026-07-05 22:26:14 +0100

    fix(role.selectors.ts): fixed selector bug

--- src/features/auth/state/selectors/role.selectors.ts
+++ src/features/auth/state/selectors/role.selectors.ts
@@ -1,4 +1,7 @@
+import { IRole } from "../../types/auth.types";
 import { IRoleState } from "../interface/auth.interface";
 
-export const selectRole = (state: IRoleState) => state.role
+export const selectRole = (state: IRoleState) => state.role;
+
+export const setRole = (state: IRoleState) => state.setRole;
 export const selectHasHydrated = (state: IRoleState) => state.hasHydrated;
