4dfdcf6b56dd69f632f79f4a4ce6589084825500
3illBaby <thrillanon@outlook.com>
2026-07-05 21:36:51 +0100

    chore(token.selectors.ts): extended selectors

--- src/features/auth/state/selectors/token.selectors.ts
+++ src/features/auth/state/selectors/token.selectors.ts
@@ -1,7 +1,12 @@
-import { ITokenState } from "../interface/auth.interface";
+import { IRoleState, ITokenState } from "../interface/auth.interface";
 
 export const selectUser = (state: ITokenState) => state.user;
 export const selectUserId = (state: ITokenState) => state.user?.id;
 export const selectAccessToken = (state: ITokenState) => state.accessToken;
-export const selectIsAuthenticated = (state: ITokenState) => state.isAuthenticated;
-export const selectHasHydrated = (state: ITokenState) => state.hasHydrated
+export const selectIsAuthenticated = (state: ITokenState) =>
+  state.isAuthenticated;
+export const selectHasHydrated = (state: ITokenState) => state.hasHydrated;
+
+export const updateUser = (state: ITokenState) => state.updateUser;
+
+export const setRole = (state: IRoleState) => state.setRole;
