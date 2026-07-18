66557ee687623dee77d5919f5d6993e3e460343d
3illBaby <thrillanon@outlook.com>
2026-07-18 16:58:10 +0100

    refactor(route.ts): streamline route

--- src/features/mail-list/api/route.ts
+++ src/features/mail-list/api/route.ts
@@ -1,20 +1,7 @@
-import axios from "axios";
-import { isProduction } from "@/src/config/environment";
-import { env } from "@/src/config/schemas/env";
-import { createUrl } from "@/src/utils/url-factory";
+import { ApiClient } from "@/src/lib/api-client";
 import { MAIL_LIST_OP } from "../data/data";
 import { IAddToMailList } from "../schemas/email.schema";
 
-function provideUrl() {
-  const config = env.env;
-  const baseUrl = isProduction() ? config.PROD_URL : config.STAGING_URL;
-  return createUrl({
-    baseUrl,
-    path: MAIL_LIST_OP.ADD_TO_MAIL_LIST,
-  });
-}
-
 export async function addToMailList(ctx: IAddToMailList) {
-  const response = await axios.post(provideUrl(), ctx);
-  return response.data;
+  return ApiClient.post(MAIL_LIST_OP.ADD_TO_MAIL_LIST, ctx);
 }
