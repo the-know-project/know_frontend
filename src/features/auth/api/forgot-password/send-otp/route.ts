69a4d058d2d8a46d2e614363f1a503108d1afdbd
3illBaby <thrillanon@outlook.com>
2026-07-18 16:54:41 +0100

    refactor(route.ts): used api client

--- src/features/auth/api/forgot-password/send-otp/route.ts
+++ src/features/auth/api/forgot-password/send-otp/route.ts
@@ -1,11 +1,11 @@
+import { ApiClient } from "@/src/lib/api-client";
 import { AUTH_OP } from "../../../data/auth.path";
 import { IForgotPassword } from "../../../types/auth.types";
-import { AuthenticatedApiClient } from "../../http/authenticated-client";
 
 export async function sendOtp(ctx: IForgotPassword) {
   if (typeof window !== "undefined") {
     sessionStorage.setItem("email", ctx.email);
   }
 
-  return await AuthenticatedApiClient.post(AUTH_OP.FORGOT_PASSWORD, ctx);
+  return await ApiClient.post(AUTH_OP.FORGOT_PASSWORD, ctx);
 }
--- src/features/auth/api/google-login/route.ts
+++ src/features/auth/api/google-login/route.ts
@@ -1,8 +1,8 @@
+import { ApiClient } from "@/src/lib/api-client";
 import { AUTH_OP } from "../../data/auth.path";
 import { IRole } from "../../types/auth.types";
-import { AuthenticatedApiClient } from "../http/authenticated-client";
 
 export function googleLogin(role: IRole) {
   const path = `${AUTH_OP.GOOGLE_LOGIN}?role=${role}`;
-  return AuthenticatedApiClient.window(path);
+  return ApiClient.window(path);
 }
