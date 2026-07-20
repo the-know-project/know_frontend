1863ed18e2cb6d7085c68476bdbb16d6398ac5e5
3illBaby <thrillanon@outlook.com>
2026-07-20 14:34:33 +0100

    chore(acrt.types.ts): extended

--- src/features/cart/types/cart.types.ts
+++ src/features/cart/types/cart.types.ts
@@ -1,7 +1,12 @@
 import { z } from "zod";
-import { CartData, CartResponseDto } from "../dto/cart.dto";
+import {
+  CartData,
+  CartResponseDto,
+  IAddToLocalCartSchema,
+} from "../dto/cart.dto";
 import { AddToCartSchema } from "../schema/cart.schema";
 
 export type TCart = z.infer<typeof CartData>;
 export type IAddToCart = z.infer<typeof AddToCartSchema>;
 export type IUserCart = z.infer<typeof CartResponseDto>;
+export type IAddToLocalCart = z.infer<typeof IAddToLocalCartSchema>;
