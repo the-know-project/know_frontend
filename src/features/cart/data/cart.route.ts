5e0b6223718e0580daae90f2400522dbdd464a39
3illBaby <thrillanon@outlook.com>
2026-07-20 15:53:46 +0100

    chore(cart.route.ts): extended path

--- src/features/cart/data/cart.route.ts
+++ src/features/cart/data/cart.route.ts
@@ -2,4 +2,5 @@ export enum CART_OP {
   ADD_TO_CART = "/api/cart/addToCart",
   REMOVE_FROM_CART = "/api/cart/deleteFromCart",
   FETCH_USER_CART = "/api/cart/userCartItems",
+  UPDATE_ITEM_QUANTITY = "/api/cart/updateCartItemQuantity",
 }
