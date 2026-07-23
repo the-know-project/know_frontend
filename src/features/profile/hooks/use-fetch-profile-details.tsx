04fce8e353443520850032f8e421bc166aef518f
3illBaby <thrillanon@outlook.com>
2026-07-23 06:27:28 +0100

    renamed file
    renamed file

--- src/features/profile/hooks/use-fetch-profile-details.tsx
+++ src/features/profile/hooks/use-fetch-profile-details.tsx
@@ -1,40 +1,35 @@
 "use client";
 
 import { useQuery } from "@tanstack/react-query";
-import { fetchArtistDetails } from "../api/fetch-user/route";
+
 import { IFetchProfileData } from "../artist/types/profile.types";
 import { useTokenStore } from "../../auth/state/store";
 import { selectUserId } from "../../auth/state/selectors/token.selectors";
-import { showLog } from "@/src/utils/logger";
 import { err, ok, ResultAsync } from "neverthrow";
 import { ProfileError } from "../error/profile.error";
+import { fetchProfile } from "../api/fetch-user/route";
 
-type IFetchArtistDetails = Omit<IFetchProfileData, "viewerId">;
-export const useFetchArtistDetails = (ctx: IFetchArtistDetails) => {
+type IFetchProfileDetails = Omit<IFetchProfileData, "viewerId">;
+export const useFetchProfileDetails = (ctx: IFetchProfileDetails) => {
   const currentUserId = useTokenStore(selectUserId);
-  showLog({
-    context: "Fetch Artist Details",
-    data: {
-      viewerId: currentUserId,
-    },
-  });
+
   return useQuery({
-    queryKey: [`artist-details${ctx.userId}-${currentUserId ?? ""}`],
+    queryKey: [`user-profile-${ctx.userId}-${currentUserId ?? ""}`],
     queryFn: async () => {
       const result = await ResultAsync.fromPromise(
-        fetchArtistDetails({
+        fetchProfile({
           userId: ctx.userId,
           viewerId: currentUserId as string,
         }),
         (error) =>
           new ProfileError(
-            `Error fetching artist details: ${error instanceof Error ? error.message : String(error)}`,
+            `Error fetching user details: ${error instanceof Error ? error.message : String(error)}`,
           ),
       ).andThen((data) => {
         if (data.status === 200) {
           return ok(data);
         } else {
-          return err(new ProfileError("Error fetching artist details"));
+          return err(new ProfileError("Error fetching user details"));
         }
       });
       if (result.isErr()) {
