5a16902920357e59e83e17112a1d3e1f61942de5
3illBaby <thrillanon@outlook.com>
2026-07-23 06:28:12 +0100

    chore(profile.types.ts): updated types

--- src/features/profile/types/profile.types.ts
+++ src/features/profile/types/profile.types.ts
@@ -1,14 +1,14 @@
 import { z } from "zod";
 import { ArtistMetricsDto, FetchUserAsset } from "../artist/dto/artist.dto";
 import {
-  ArtistDetailsDtoSchema,
+  UserProfileSchema,
   ProfileFormSchema,
   UpdateProfileResponseDto,
 } from "../dto/profile.dto";
 
 export type IArtistMetricsDto = z.infer<typeof ArtistMetricsDto>;
 export type IFetchUserAsset = z.infer<typeof FetchUserAsset>;
-export type TArtistDetailsDto = z.infer<typeof ArtistDetailsDtoSchema>;
+export type TUserProfileDto = z.infer<typeof UserProfileSchema>;
 export type ProfileFormData = z.infer<typeof ProfileFormSchema>;
 export type IUpdateProfileResponseDto = z.infer<
   typeof UpdateProfileResponseDto
