e002d6a868f432143ea227d2ef0e8f9ddb760cb2
3illBaby <thrillanon@outlook.com>
2026-07-23 06:30:31 +0100

    update dto

--- src/features/profile/dto/profile.dto.ts
+++ src/features/profile/dto/profile.dto.ts
@@ -1,19 +1,20 @@
 import { z } from "zod";
 
-export const ArtistDetailsDataSchema = z.object({
+export const UserProfileDataSchema = z.object({
   id: z.string(),
   firstName: z.string(),
   lastName: z.string(),
+  description: z.string(),
   email: z.string().email(),
   imageUrl: z.string().url(),
   username: z.string(),
   role: z.string(),
 });
 
-export const ArtistDetailsDtoSchema = z.object({
+export const UserProfileSchema = z.object({
   status: z.number(),
   message: z.string(),
-  data: ArtistDetailsDataSchema,
+  data: UserProfileDataSchema,
   meta: z.any().nullable(),
 });
 
