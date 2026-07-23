b273763095dc864b3790f95cf4bb476a332a46e7
3illBaby <thrillanon@outlook.com>
2026-07-23 06:22:21 +0100

    refactor(route.ts): renamed func

--- src/features/profile/api/fetch-user/route.ts
+++ src/features/profile/api/fetch-user/route.ts
@@ -1,12 +1,13 @@
 import { ApiClient } from "@/src/lib/api-client";
-import { TArtistDetailsDto } from "../../types/profile.types";
+
 import { IFetchProfileData } from "../../artist/types/profile.types";
+import { TUserProfileDto } from "../../types/profile.types";
 
 const ARTIST_PROFILE_ENDPOINT = "/api/user/userById";
 
-export async function fetchArtistDetails(
+export async function fetchProfile(
   ctx: IFetchProfileData,
-): Promise<TArtistDetailsDto> {
+): Promise<TUserProfileDto> {
   const path = `${ARTIST_PROFILE_ENDPOINT}?userId=${ctx.userId as string}&viewerId=${ctx.viewerId as string}`;
   return await ApiClient.get(path);
 }
