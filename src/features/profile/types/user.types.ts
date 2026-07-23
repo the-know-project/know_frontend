010ac62babc3a5df8b1b8a12aa7552b3a551159e
3illBaby <thrillanon@outlook.com>
2026-07-23 06:55:54 +0100

    update types

--- src/features/profile/types/user.types.ts
+++ src/features/profile/types/user.types.ts
@@ -52,7 +52,7 @@ export const UpdateProfileRequestSchema = z.object({
   oldPassword: z.string().optional(),
   newPassword: z.string().optional(),
   profilePicture: z.instanceof(File).optional(),
-  role: z.enum(["ARTIST", "BUYER"]).optional(),
+  role: z.string().optional(),
 });
 
 export type IUpdateProfileRequest = z.infer<typeof UpdateProfileRequestSchema>;
