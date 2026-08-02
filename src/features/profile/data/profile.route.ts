8dac491ffe1ff4b1292ea5f46f6e6f5c65940039
3illBaby <thrillanon@outlook.com>
2026-08-02 07:02:39 +0100

    defined routes

--- src/features/profile/data/profile.route.ts
+++ src/features/profile/data/profile.route.ts
@@ -1,5 +1,6 @@
 export enum PROFILE_OP {
   USER_ASSETS = "/api/asset/userAsset",
   UPDATE_PROFILE = "/api/user/updateUser",
+  LIST_ASSET = "/api/asset/listAsset",
+  UNLIST_ASSET = "/api/asset/unlistAsset",
 }
-
