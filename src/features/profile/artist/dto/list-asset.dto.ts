d1ae19d4f0b705013702b9959acd0f035a2e7056
3illBaby <thrillanon@outlook.com>
2026-08-04 15:11:36 +0100

    chore: extended

--- src/features/profile/artist/dto/list-asset.dto.ts
+++ src/features/profile/artist/dto/list-asset.dto.ts
@@ -3,5 +3,6 @@ import { z } from "zod";
 export const ListAssetDto = z.object({
   fileId: z.string(),
   price: z.string(),
+  quantity: z.number().min(1, "Quantity must be at least 1"),
   currency: z.string(),
 });
