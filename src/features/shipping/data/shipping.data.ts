2bc8a2eb34303c28e87d5183e749292a48bfe081
3illBaby <thrillanon@outlook.com>
2026-08-06 23:39:01 +0100

    chore: Define shipping routes

---
+++ src/features/shipping/data/shipping.data.ts
@@ -0,0 +1,6 @@
+export enum SHIPPING_ROUTES {
+  CREATE_SHIPPING_INFO = "/api/shipping/createInfo",
+  FETCH_SHIPPING_INFO = "/api/shipping/info",
+  UPDATE_SHIPPING_INFO = "/api/shipping/updateInfo",
+  DELETE_SHIPPING_INFO = "/api/shipping/deleteInfo",
+}
