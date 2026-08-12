1ce01c43224f4bc7e34a8728b8be6ba46027c419
3illBaby <thrillanon@outlook.com>
2026-08-12 13:45:04 +0100

    chore: extend route list

--- src/features/cart/data/cart.route.ts
+++ src/features/cart/data/cart.route.ts
@@ -3,4 +3,5 @@ export enum CART_OP {
   REMOVE_FROM_CART = "/api/cart/deleteFromCart",
   FETCH_USER_CART = "/api/cart/userCartItems",
   UPDATE_ITEM_QUANTITY = "/api/cart/updateCartItemQuantity",
+  CLEAR_CART = "/api/cart/clearUserCart",
 }
