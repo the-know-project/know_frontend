766fa7804562e00d9d13b3f24a08b7bff9e4d2a4
3illBaby <thrillanon@outlook.com>
2026-08-19 15:29:45 +0100

    chore: extended order types

--- src/features/orders/types/orders.types.ts
+++ src/features/orders/types/orders.types.ts
@@ -7,6 +7,8 @@ import {
   OrdersData,
   OrdersSummaryDto,
   OrdersByIdDto,
+  OrderItems,
+  CreateOrderResponseDto,
 } from "../dto/orders.dto";
 
 export type ICreateOrder = z.infer<typeof CreateOrderDto>;
@@ -16,3 +18,7 @@ export type TOrdersData = z.infer<typeof OrdersData>;
 export type UserOrdersResponse = z.infer<typeof FetchUserOrdersDto>;
 export type OrderSummaryResponse = z.infer<typeof OrdersSummaryDto>;
 export type OrdersByIdResponse = z.infer<typeof OrdersByIdDto>;
+
+export type IOrderItems = z.infer<typeof OrderItems>;
+
+export type ICreateOrderResponse = z.infer<typeof CreateOrderResponseDto>;
