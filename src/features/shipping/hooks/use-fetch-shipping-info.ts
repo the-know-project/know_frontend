c5caab241152d7a99401c2005089582da9436bdb
3illBaby <thrillanon@outlook.com>
2026-08-19 15:35:25 +0100

    feat: Implement hook to fetch shipping info

--- src/features/shipping/hooks/use-fetch-shipping-info.ts
+++ src/features/shipping/hooks/use-fetch-shipping-info.ts
@@ -2,11 +2,30 @@ import { useQuery } from "@tanstack/react-query";
 import { err, ok, ResultAsync } from "neverthrow";
 import { useTokenStore } from "../../auth/state/store";
 import { selectUserId } from "../../auth/state/selectors/token.selectors";
+import { fetchShippingInfo } from "../api/fetch-shipping-info/route";
 
 export const useFetchShippingInfo = () => {
   const userId = useTokenStore(selectUserId);
   return useQuery({
     queryKey: [`fetch-shipping-info-${userId}`],
-    queryFn: async () => {},
+    queryFn: async () => {
+      const result = await ResultAsync.fromPromise(
+        fetchShippingInfo(),
+        (error) =>
+          new Error(`An error occurred while fetching shipping info ${error}`),
+      ).andThen((data) => {
+        if (data.status === 200) {
+          return ok(data);
+        }
+
+        return err(new Error(`${data.message}`));
+      });
+
+      if (result.isErr()) {
+        throw result.error;
+      }
+
+      return result.value as ICreateShippingResponse;
+    },
   });
 };
